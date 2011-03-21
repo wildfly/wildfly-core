0fd4984a73c7859f2ad98be50b034495aa9b8482
Darran Lofthouse <darran.lofthouse@jboss.com>
2011-03-21 15:38:05 +0000

    [AS7-399] Adding authenticators to the domain management HTTP server and introducing an out of the box configuration embedded in the domain model.
    [AS7-760] Adding an LDAP based CallbackHandler along with configuration.
              Authenticator selection is now based on checking the capabilities of the CallbackHandler.
    
    [AS7-729] Added configuration and initialisation to enable an SSL socket for the HTTP server.
    
    [AS7-792] New artefact / module for domain management operation handlers and services.
    
    was: ff4c6fce892b3685c48cfbb22219470d0dbcb2f5

---
+++ domain-management/src/main/java/org/jboss/as/domain/management/SecurityRealm.java
@@ -0,0 +1,53 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.domain.management;
+
+import javax.net.ssl.SSLContext;
+
+import org.jboss.as.domain.management.security.DomainCallbackHandler;
+
+/**
+ * Interface to the security realm.
+ *
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+public interface SecurityRealm {
+
+    /**
+     * @return The name of this SecurityRealm
+     */
+    String getName();
+
+    /**
+     * @return The CallbackHandler for the realm
+     */
+    DomainCallbackHandler getCallbackHandler();
+
+    /**
+     * Used to obtain the SSLContext as configured for this security realm.
+     *
+     * @return the SSLContext server identity for this realm.
+     * @throws IllegalStateException - If no SSL server-identity has been defined.
+     */
+    SSLContext getSSLContext();
+
+}
