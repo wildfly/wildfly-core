90690673685898f3778646baebd1b42583cb465a
Tomaz Cerar <tomaz.cerar@gmail.com>
2013-04-10 12:55:42 +0200

    AS7-6868 - Introduce IO subsystem
    this is split from undertow subsystem so xnio configuration & friends can be all in same place for everyone to use
    
    was: 3d345fab4e333d64bc78b0fd2834d88cc225d2b5

---
+++ io/src/main/java/org/jboss/as/io/BufferPoolResourceDefinition.java
@@ -0,0 +1,125 @@
+/*
+ *
+ *  JBoss, Home of Professional Open Source.
+ *  Copyright 2013, Red Hat, Inc., and individual contributors
+ *  as indicated by the @author tags. See the copyright.txt file in the
+ *  distribution for a full listing of individual contributors.
+ *
+ *  This is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this software; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * /
+ */
+
+package org.jboss.as.io;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.jboss.as.controller.AbstractAddStepHandler;
+import org.jboss.as.controller.AttributeDefinition;
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.ReloadRequiredRemoveStepHandler;
+import org.jboss.as.controller.ReloadRequiredWriteAttributeHandler;
+import org.jboss.as.controller.ServiceVerificationHandler;
+import org.jboss.as.controller.SimpleAttributeDefinition;
+import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
+import org.jboss.as.controller.SimplePersistentResourceDefinition;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.ModelType;
+import org.jboss.msc.service.ServiceBuilder;
+import org.jboss.msc.service.ServiceController;
+import org.xnio.Pool;
+
+/**
+ * @author <a href="mailto:tomaz.cerar@redhat.com">Tomaz Cerar</a> (c) 2013 Red Hat Inc.
+ */
+class BufferPoolResourceDefinition extends SimplePersistentResourceDefinition {
+
+    static final SimpleAttributeDefinition BUFFER_SIZE = new SimpleAttributeDefinitionBuilder(Constants.BUFFER_SIZE, ModelType.INT)
+            .setDefaultValue(new ModelNode(1024))
+            .build();
+    static final SimpleAttributeDefinition BUFFER_PER_SLICE = new SimpleAttributeDefinitionBuilder(Constants.BUFFER_PER_SLICE, ModelType.INT)
+            .setDefaultValue(new ModelNode(1024))
+            .build();
+
+
+    /*<buffer-pool name="default" buffer-size="1024" buffers-per-slice="1024"/>*/
+
+    static List<SimpleAttributeDefinition> ATTRIBUTES = Arrays.asList(
+            BUFFER_SIZE,
+            BUFFER_PER_SLICE
+    );
+
+
+    public static final BufferPoolResourceDefinition INSTANCE = new BufferPoolResourceDefinition();
+
+
+    private BufferPoolResourceDefinition() {
+        super(IOExtension.BUFFER_POOL_PATH,
+                IOExtension.getResolver(Constants.BUFFER_POOL),
+                new BufferPoolAdd(),
+                ReloadRequiredRemoveStepHandler.INSTANCE
+        );
+    }
+
+    @Override
+    public Collection<AttributeDefinition> getAttributes() {
+        return (Collection) ATTRIBUTES;
+    }
+
+    @Override
+    public void registerAttributes(ManagementResourceRegistration resourceRegistration) {
+        super.registerAttributes(resourceRegistration);
+        for (AttributeDefinition attr : BufferPoolResourceDefinition.ATTRIBUTES) {
+            resourceRegistration.registerReadWriteAttribute(attr, null, new ReloadRequiredWriteAttributeHandler(attr));
+        }
+    }
+
+    private static class BufferPoolAdd extends AbstractAddStepHandler {
+        @Override
+        protected void populateModel(ModelNode operation, ModelNode model) throws OperationFailedException {
+            for (AttributeDefinition attr : BufferPoolResourceDefinition.ATTRIBUTES) {
+                attr.validateAndSet(operation, model);
+            }
+        }
+
+        @Override
+        protected void performRuntime(OperationContext context, ModelNode operation, ModelNode model, ServiceVerificationHandler verificationHandler, List<ServiceController<?>> newControllers) throws OperationFailedException {
+            final PathAddress address = PathAddress.pathAddress(operation.get(OP_ADDR));
+            final String name = address.getLastElement().getValue();
+            int bufferSize = BUFFER_SIZE.resolveModelAttribute(context, model).asInt();
+            int bufferPerSlice = BUFFER_PER_SLICE.resolveModelAttribute(context, model).asInt();
+
+            final BufferPoolService service = new BufferPoolService(bufferSize, bufferPerSlice);
+            final ServiceBuilder<Pool<ByteBuffer>> serviceBuilder = context.getServiceTarget().addService(IOServices.BUFFER_POOL.append(name), service);
+
+            serviceBuilder.setInitialMode(ServiceController.Mode.ACTIVE);
+
+            final ServiceController<Pool<ByteBuffer>> serviceController = serviceBuilder.install();
+            if (newControllers != null) {
+                newControllers.add(serviceController);
+            }
+
+
+        }
+    }
+}
