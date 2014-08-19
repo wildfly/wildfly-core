28d2eb361cfb0c722de5b26841e8e9318e1f56c3
Claudio Miranda <claudio@claudius.com.br>
2014-08-19 00:18:47 -0300

    fix year of copyright. Only initialize ConnectionInfoBean after connection is sucessfull

--- cli/src/main/java/org/jboss/as/cli/impl/CommandContextImpl.java
+++ cli/src/main/java/org/jboss/as/cli/impl/CommandContextImpl.java
@@ -843,8 +843,6 @@ class CommandContextImpl implements CommandContext, ModelControllerClientFactory
         do {
             try {
                 CallbackHandler cbh = new AuthenticationCallbackHandler(username, password);
-                connInfoBean = new ConnectionInfoBean();
-                connInfoBean.setDisableLocalAuth(disableLocalAuth);
                 if (log.isDebugEnabled()) {
                     log.debug("connecting to " + address.getHost() + ':' + address.getPort() + " as " + username);
                 }
@@ -853,6 +851,8 @@ class CommandContextImpl implements CommandContext, ModelControllerClientFactory
                 retry = false;
                 tryConnection(tempClient, address);
                 initNewClient(tempClient, address);
+                connInfoBean = new ConnectionInfoBean();
+                connInfoBean.setDisableLocalAuth(disableLocalAuth);
                 connInfoBean.setLoggedSince(new Date());
             } catch (RedirectException re) {
                 try {
