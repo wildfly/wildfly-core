7bcb480f8f8a6455925ddd31cddcbf51f40dd2cf
James Perkins <jperkins@redhat.com>
2014-10-09 15:00:50 -0700

    [WFCORE-158] Don't allow handlers to be removed if assigned to a logger or another handler.

--- logging/src/main/java/org/jboss/as/logging/HandlerOperations.java
+++ logging/src/main/java/org/jboss/as/logging/HandlerOperations.java
@@ -36,6 +36,7 @@ import static org.jboss.as.logging.CommonAttributes.HANDLER_NAME;
 import static org.jboss.as.logging.CommonAttributes.LEVEL;
 import static org.jboss.as.logging.CommonAttributes.MODULE;
 import static org.jboss.as.logging.CommonAttributes.PROPERTIES;
+import static org.jboss.as.logging.CommonAttributes.ROOT_LOGGER_NAME;
 import static org.jboss.as.logging.Logging.createOperationFailure;
 import static org.jboss.as.logging.PatternFormatterResourceDefinition.PATTERN;
 
@@ -67,6 +68,7 @@ import org.jboss.logmanager.Logger.AttachmentKey;
 import org.jboss.logmanager.config.FormatterConfiguration;
 import org.jboss.logmanager.config.HandlerConfiguration;
 import org.jboss.logmanager.config.LogContextConfiguration;
+import org.jboss.logmanager.config.LoggerConfiguration;
 import org.jboss.logmanager.config.PojoConfiguration;
 import org.jboss.logmanager.config.PropertyConfigurable;
 import org.jboss.logmanager.formatters.PatternFormatter;
@@ -436,8 +438,40 @@ final class HandlerOperations {
 
         @Override
         public void performRuntime(final OperationContext context, final ModelNode operation, final LogContextConfiguration logContextConfiguration, final String name, final ModelNode model) throws OperationFailedException {
-            // Validating wouldn't work until the LogContextConfiguration.commit() happens as handlers could still be
-            // named as attached removed the check for this reason.
+            // Check that the handler is not assigned to a logger
+            final List<String> loggerNames = logContextConfiguration.getLoggerNames();
+            final List<String> assigned = new ArrayList<>();
+            for (String loggerName : loggerNames) {
+                final LoggerConfiguration c = logContextConfiguration.getLoggerConfiguration(loggerName);
+                if (c != null) {
+                    if (c.getHandlerNames().contains(name)) {
+                        if (ROOT_LOGGER_NAME.equals(loggerName)) {
+                            assigned.add(RootLoggerResourceDefinition.ROOT_LOGGER_ATTRIBUTE_NAME);
+                        } else {
+                            assigned.add(loggerName);
+                        }
+                    }
+                }
+            }
+            if (!assigned.isEmpty()) {
+                context.setRollbackOnly();
+                throw LoggingLogger.ROOT_LOGGER.handlerAttachedToLoggers(name, assigned);
+            }
+
+            // Check for handlers that haven been assigned the handler that is attempting to be removed
+            final List<String> handlerNames = logContextConfiguration.getHandlerNames();
+            for (String handlerName : handlerNames) {
+                final HandlerConfiguration c = logContextConfiguration.getHandlerConfiguration(handlerName);
+                if (c != null) {
+                    if (c.getHandlerNames().contains(name)) {
+                        assigned.add(handlerName);
+                    }
+                }
+            }
+            if (!assigned.isEmpty()) {
+                context.setRollbackOnly();
+                throw LoggingLogger.ROOT_LOGGER.handlerAttachedToHandlers(name, assigned);
+            }
 
             // Remove the handler
             logContextConfiguration.removeHandlerConfiguration(name);
