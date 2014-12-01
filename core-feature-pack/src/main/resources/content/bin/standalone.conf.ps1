9e338de1bb6cb0681a9c0fd013ce9d64716d9066
Tomaz Cerar <tomaz.cerar@gmail.com>
2014-12-01 12:14:35 +0100

    [JDK8] Don't use permgen anymore

--- core-feature-pack/src/main/resources/content/bin/standalone.conf.ps1
+++ core-feature-pack/src/main/resources/content/bin/standalone.conf.ps1
@@ -51,7 +51,6 @@ $JAVA_OPTS = @()
 # JVM memory allocation pool parameters - modify as appropriate.
 $JAVA_OPTS += '-Xms64M'
 $JAVA_OPTS += '-Xmx512M'
-$JAVA_OPTS += '-XX:MaxPermSize=256M'
 
 # Reduce the RMI GCs to once per hour for Sun JVMs.
 $JAVA_OPTS += '-Dsun.rmi.dgc.client.gcInterval=3600000'
