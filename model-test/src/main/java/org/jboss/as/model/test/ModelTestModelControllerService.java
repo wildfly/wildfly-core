82b886c8d516e92649ed92e2afb07b116939dcc2
Brian Stansberry <brian.stansberry@redhat.com>
2015-02-23 20:53:02 -0600

    Revert "Merge pull request #519 from bstansberry/revert-legacy-Alpha5"
    This reverts commit 88365641486d8deaa47ae4e4af6b365f07ae0b25, reversing
    changes made to 59d49fd58d038561d4115402d1b78c1e66192a58.

--- model-test/src/main/java/org/jboss/as/model/test/ModelTestModelControllerService.java
+++ model-test/src/main/java/org/jboss/as/model/test/ModelTestModelControllerService.java
@@ -24,6 +24,7 @@ package org.jboss.as.model.test;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DESCRIPTION;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
 
+import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -89,7 +90,7 @@ public abstract class ModelTestModelControllerService extends AbstractController
                            final DescriptionProvider rootDescriptionProvider, ControlledProcessState processState, Controller71x version) {
         // Fails in core-model-test transformation testing if ExpressionResolver.TEST_RESOLVER is used because not present in 7.1.x
         super(processType, runningModeControl, persister,
-                processState == null ? new ControlledProcessState(true) : processState, rootDescriptionProvider, null, ExpressionResolver.TEST_RESOLVER);
+                processState == null ? new ControlledProcessState(true) : processState, rootDescriptionProvider, null, getExpressionResolverFor71());
         this.persister = persister;
         this.transformerRegistry = transformerRegistry;
         this.validateOpsFilter = validateOpsFilter;
@@ -108,6 +109,15 @@ public abstract class ModelTestModelControllerService extends AbstractController
         this.transformerRegistry = transformerRegistry;
         this.validateOpsFilter = validateOpsFilter;
         this.runningModeControl = runningModeControl;
+    }
+
+    private static ExpressionResolver getExpressionResolverFor71() {
+        try {
+            Field defaultExpressionResolver = ExpressionResolver.class.getDeclaredField("DEFAULT");
+            return (ExpressionResolver) defaultExpressionResolver.get(null);
+        } catch (NoSuchFieldException ex) {
+        } catch (IllegalAccessException ex) {}
+        return ExpressionResolver.TEST_RESOLVER;
     }
 
     /**
