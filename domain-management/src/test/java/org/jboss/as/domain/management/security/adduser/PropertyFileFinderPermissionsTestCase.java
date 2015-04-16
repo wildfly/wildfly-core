4edef08fe17e173480ca9f9b549cff8479bd2847
bgaisford <bgaisford@punagroup.com>
2015-04-16 06:27:01 -1000

    WFCORE-640 Added property file finder permissions test case.

--- domain-management/src/test/java/org/jboss/as/domain/management/security/adduser/PropertyFileFinderPermissionsTestCase.java
+++ domain-management/src/test/java/org/jboss/as/domain/management/security/adduser/PropertyFileFinderPermissionsTestCase.java
@@ -23,26 +23,20 @@
 package org.jboss.as.domain.management.security.adduser;
 
 import org.jboss.as.domain.management.security.adduser.AddUser.FileMode;
-import org.jboss.msc.service.StartException;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 import static java.lang.System.getProperty;
-import static java.lang.System.setProperty;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Test the property file finder.
+ * Test the property file finder permissions tests.
  *
- * @author <a href="mailto:flemming.harms@gmail.com">Flemming Harms</a>
+ * @author <a href="mailto:bgaisford@punagroup.com">Brandon Gaisford</a>
  */
 public class PropertyFileFinderPermissionsTestCase extends PropertyTestHelper {
 
@@ -50,15 +44,18 @@ public class PropertyFileFinderPermissionsTestCase extends PropertyTestHelper {
     @Before
     public void setup() throws IOException {
         values.setFileMode(FileMode.MANAGEMENT);
-        values.getOptions().setJBossHome(getProperty("java.io.tmpdir"));
+        values.getOptions().setJBossHome(getProperty("java.io.tmpdir")+File.separator+"permmissions");
     }
 
     private File createPropertyFile(String filename, String mode) throws IOException {
 
-        File domainDir = new File(getProperty("java.io.tmpdir")+File.separator+mode);
-        domainDir.mkdir();
-        domainDir.deleteOnExit();
-        File propertyUserFile = new File(domainDir, filename);
+        File permissionsDir = new File(getProperty("java.io.tmpdir")+File.separator+"permmissions");
+        permissionsDir.mkdir();
+        permissionsDir.deleteOnExit();
+        File parentDir = new File(permissionsDir.getPath()+File.separator+mode);
+        parentDir.mkdir();
+        parentDir.deleteOnExit();
+        File propertyUserFile = new File(parentDir, filename);
         propertyUserFile.createNewFile();
         propertyUserFile.deleteOnExit();
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(propertyUserFile), StandardCharsets.UTF_8));
@@ -72,67 +69,9 @@ public class PropertyFileFinderPermissionsTestCase extends PropertyTestHelper {
         return propertyUserFile;
     }
 
-    @Test
-    public void overridePropertyfileLocationRead() throws IOException {
-        File domainMgmtUserFile = createPropertyFile("mgmt-users.properties", "domain");
-        File standaloneMgmtUserFile = createPropertyFile("mgmt-users.properties", "standalone");
-        File domainMgmtGroupFile = createPropertyFile("mgmt-groups.properties", "domain");
-        File standaloneMgmtGroupFile = createPropertyFile("mgmt-groups.properties", "standalone");
-
-        System.setProperty("jboss.server.config.user.dir", standaloneMgmtUserFile.getParent());
-        System.setProperty("jboss.domain.config.user.dir", domainMgmtUserFile.getParent());
-        System.setProperty("jboss.server.config.group.dir", standaloneMgmtGroupFile.getParent());
-        System.setProperty("jboss.domain.config.group.dir", domainMgmtGroupFile.getParent());
-        State propertyFileFinder = new PropertyFileFinder(consoleMock, values);
-        State nextState = propertyFileFinder.execute();
-        assertTrue(nextState instanceof PromptRealmState);
-        assertTrue("Expected to find the " + USER_NAME + " in the list of known enabled users", values.getEnabledKnownUsers().contains(USER_NAME));
-        assertTrue("Expected the values.getPropertiesFiles() contained the "+standaloneMgmtUserFile,values.getUserFiles().contains(standaloneMgmtUserFile));
-        assertTrue("Expected the values.getPropertiesFiles() contained the "+domainMgmtUserFile,values.getUserFiles().contains(domainMgmtUserFile));
-    }
-
-    @Test
-    public void overridePropertyfileLocationWrite() throws IOException, StartException {
-        File domainUserFile = createPropertyFile("application-users.properties", "domain");
-        File standaloneUserFile = createPropertyFile("application-users.properties", "standalone");
-        File domainRolesFile = createPropertyFile("application-roles.properties", "domain");
-        File standaloneRolesFile = createPropertyFile("application-roles.properties", "standalone");
-
-        String newUserName = "Hugh.Jackman";
-        values.setGroups(null);
-        values.setUserName(newUserName);
-        values.setFileMode(FileMode.APPLICATION);
-        System.setProperty("jboss.server.config.user.dir", domainUserFile.getParent());
-        System.setProperty("jboss.domain.config.user.dir", standaloneUserFile.getParent());
-        System.setProperty("jboss.server.config.role.dir", domainRolesFile.getParent());
-        System.setProperty("jboss.domain.config.role.dir", standaloneRolesFile.getParent());
-        State propertyFileFinder = new PropertyFileFinder(consoleMock, values);
-        State nextState = propertyFileFinder.execute();
-        assertTrue(nextState instanceof PromptRealmState);
-
-        File locatedDomainPropertyFile = values.getUserFiles().get(values.getUserFiles().indexOf(domainUserFile));
-        File locatedStandalonePropertyFile = values.getUserFiles().get(values.getUserFiles().indexOf(standaloneUserFile));
-        UpdateUser updateUserState = new UpdateUser(consoleMock, values);
-
-        AssertConsoleBuilder consoleBuilder = new AssertConsoleBuilder().
-                expectedDisplayText(updateUserState.consoleUserMessage(locatedDomainPropertyFile.getCanonicalPath())).
-                expectedDisplayText(AddUser.NEW_LINE).
-                expectedDisplayText(updateUserState.consoleUserMessage(locatedStandalonePropertyFile.getCanonicalPath())).
-                expectedDisplayText(AddUser.NEW_LINE);
-        consoleMock.setResponses(consoleBuilder);
-        updateUserState.update(values);
-
-        assertUserPropertyFile(newUserName);
-        consoleBuilder.validate();
-    }
 
     @Test
     public void testPropertyFileFinderFilePermissions() throws IOException {
-
-        String oldJavaTempDir = getProperty("java.io.tmpdir");
-
-        setProperty("java.io.tmpdir", oldJavaTempDir+File.separator+"permissions");
-        values.getOptions().setJBossHome(getProperty("java.io.tmpdir"));
 
         File domainMgmtUserFile = createPropertyFile("mgmt-users.properties", "domain");
         File standaloneMgmtUserFile = createPropertyFile("mgmt-users.properties", "standalone");
@@ -154,33 +93,27 @@ public class PropertyFileFinderPermissionsTestCase extends PropertyTestHelper {
             standaloneDir.setReadable(true);
         }
 
-//        File permissionsMgmtUserFile2 = createPropertyFile("mgmt-users.properties", "permissions2");
-//        File permissionsMgmtGroupFile2 = createPropertyFile("mgmt-groups.properties", "permissions2");
-//        System.setProperty("jboss.domain.config.user.dir", permissionsMgmtUserFile2.getParent());
-//        System.setProperty("jboss.domain.config.group.dir", permissionsMgmtGroupFile2.getParent());
-//
-//        File permissionsDir2 = permissionsMgmtUserFile2.getParentFile();
-//        State propertyFileFinder2 = new PropertyFileFinder(consoleMock, values);
-//
-//        // Test parent dir without execute
-//        if(permissionsDir2.setExecutable(false)) {
-//            State nextState = propertyFileFinder2.execute();
-//            assertTrue(nextState instanceof ErrorState);
-//            permissionsDir2.setExecutable(true);
-//        }
+        // Test parent dir without execute
+        if(standaloneDir.setExecutable(false)) {
+            State nextState = propertyFileFinder.execute();
+            assertTrue(nextState instanceof ErrorState);
+            standaloneDir.setExecutable(true);
+        }
 
+        // Test file without read
+        if(standaloneMgmtUserFile.setReadable(false)) {
+            State nextState = propertyFileFinder.execute();
+            assertTrue(nextState instanceof ErrorState);
+            standaloneMgmtUserFile.setReadable(true);
+        }
 
-        // Test properties file not writable
-//        if(permissionsMgmtUserFile.setWritable(false)) {
-//
-//        }
-
-
-        // be sure to restore java temp dir
-        setProperty("java.io.tmpdir", oldJavaTempDir);
-        values.getOptions().setJBossHome(getProperty("java.io.tmpdir"));
+        // Test file without write
+        if(standaloneMgmtUserFile.setWritable(false)) {
+            State nextState = propertyFileFinder.execute();
+            assertTrue(nextState instanceof ErrorState);
+            standaloneMgmtUserFile.setWritable(true);
+        }
 
     }
-
 
 }
