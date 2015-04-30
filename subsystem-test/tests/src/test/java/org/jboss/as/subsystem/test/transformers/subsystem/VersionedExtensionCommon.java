3768ce2239f2d58139b40f081b3b513c14cf1cc4
Kabir Khan <kkhan@redhat.com>
2015-04-30 13:01:54 +0200

    Clean up transformers test

--- subsystem-test/tests/src/test/java/org/jboss/as/subsystem/test/transformers/subsystem/VersionedExtensionCommon.java
+++ subsystem-test/tests/src/test/java/org/jboss/as/subsystem/test/transformers/subsystem/VersionedExtensionCommon.java
@@ -142,6 +142,8 @@ public abstract class VersionedExtensionCommon implements Extension {
             super(def);
         }
 
+
+
         @Override
         protected boolean applyUpdateToRuntime(OperationContext context, ModelNode operation, String attributeName, ModelNode resolvedValue, ModelNode currentValue, HandbackHolder<Void> voidHandbackHolder) throws OperationFailedException {
             return false;
