5721f2b7c47405cd1716f64de8af870548ad4164
Richard Opalka <opalka.richard@gmail.com>
2015-05-15 18:13:26 +0200

    [WFCORE-651] Porting jboss-as-remoting_1_2.xsd to WildFly Core + providing test case

---
+++ remoting/subsystem/src/main/java/org/jboss/as/remoting/RemotingSubsystem12Parser.java
@@ -0,0 +1,147 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.remoting;
+
+import org.jboss.dmr.ModelNode;
+import org.jboss.staxmapper.XMLElementReader;
+import org.jboss.staxmapper.XMLExtendedStreamReader;
+
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import java.util.EnumSet;
+import java.util.List;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.parsing.ParseUtils.missingRequired;
+import static org.jboss.as.controller.parsing.ParseUtils.readStringAttributeElement;
+import static org.jboss.as.controller.parsing.ParseUtils.requireNoNamespaceAttribute;
+import static org.jboss.as.controller.parsing.ParseUtils.unexpectedAttribute;
+import static org.jboss.as.controller.parsing.ParseUtils.unexpectedElement;
+import static org.jboss.as.remoting.CommonAttributes.AUTHENTICATION_PROVIDER;
+import static org.jboss.as.remoting.CommonAttributes.CONNECTOR;
+import static org.jboss.as.remoting.CommonAttributes.SECURITY_REALM;
+import static org.jboss.as.remoting.CommonAttributes.SASL_PROTOCOL;
+import static org.jboss.as.remoting.CommonAttributes.SERVER_NAME;
+import static org.jboss.as.remoting.CommonAttributes.SOCKET_BINDING;
+
+/**
+ * Parser for remoting subsystem 1.2 version
+ *
+ * @author opalka.richard@gmail.com
+ */
+class RemotingSubsystem12Parser extends RemotingSubsystem11Parser implements XMLStreamConstants, XMLElementReader<List<ModelNode>> {
+
+    static final RemotingSubsystem12Parser INSTANCE = new RemotingSubsystem12Parser();
+
+    void parseConnector(final XMLExtendedStreamReader reader, final ModelNode address, final List<ModelNode> list) throws XMLStreamException {
+
+        String name = null;
+        String securityRealm = null;
+        String socketBinding = null;
+        String serverName = null;
+        String saslProtocol = null;
+        final EnumSet<Attribute> required = EnumSet.of(Attribute.NAME, Attribute.SOCKET_BINDING);
+        final int count = reader.getAttributeCount();
+        final ModelNode connector = new ModelNode();
+        for (int i = 0; i < count; i++) {
+            requireNoNamespaceAttribute(reader, i);
+            final String value = reader.getAttributeValue(i);
+            final Attribute attribute = Attribute.forName(reader.getAttributeLocalName(i));
+            required.remove(attribute);
+            switch (attribute) {
+                case NAME: {
+                    name = value;
+                    break;
+                }
+                case SECURITY_REALM: {
+                    securityRealm = value;
+                    break;
+                }
+                case SOCKET_BINDING: {
+                    socketBinding = value;
+                    break;
+                }
+                case SERVER_NAME: {
+                    ConnectorCommon.SERVER_NAME.parseAndSetParameter(value, connector, reader);
+                    break;
+                }
+                case SASL_PROTOCOL: {
+                    ConnectorCommon.SASL_PROTOCOL.parseAndSetParameter(value, connector, reader);
+                    break;
+                }
+                default:
+                    throw unexpectedAttribute(reader, i);
+            }
+        }
+        if (!required.isEmpty()) {
+            throw missingRequired(reader, required);
+        }
+        assert name != null;
+        assert socketBinding != null;
+
+        connector.get(OP).set(ADD);
+        connector.get(OP_ADDR).set(address).add(CONNECTOR, name);
+        // requestProperties.get(NAME).set(name); // Name is part of the address
+        connector.get(SOCKET_BINDING).set(socketBinding);
+        if (securityRealm != null) {
+            connector.get(SECURITY_REALM).set(securityRealm);
+        }
+        if (serverName != null) {
+            connector.get(SERVER_NAME).set(serverName);
+        }
+        if (saslProtocol != null) {
+            connector.get(SASL_PROTOCOL).set(saslProtocol);
+        }
+        list.add(connector);
+
+        // Handle nested elements.
+        final EnumSet<Element> visited = EnumSet.noneOf(Element.class);
+        while (reader.hasNext() && reader.nextTag() != END_ELEMENT) {
+            final Element element = Element.forName(reader.getLocalName());
+            if (visited.contains(element)) {
+                throw unexpectedElement(reader);
+            }
+            visited.add(element);
+            switch (element) {
+                case SASL: {
+                    parseSaslElement(reader, connector.get(OP_ADDR), list);
+                    break;
+                }
+                case PROPERTIES: {
+                    parseProperties(reader, connector.get(OP_ADDR), list);
+                    break;
+                }
+                case AUTHENTICATION_PROVIDER: {
+                    connector.get(AUTHENTICATION_PROVIDER).set(readStringAttributeElement(reader, "name"));
+                    break;
+                }
+                default: {
+                    throw unexpectedElement(reader);
+                }
+            }
+        }
+    }
+
+}
