fe7b1d8274522fc245d9df1a86af7ff7c6532d14
Jason T. Greene <jason.greene@redhat.com>
2015-09-06 15:32:33 -0500

    WFCORE-963 Add capability for IO worker thread pool size

--- io/subsystem/src/main/java/org/wildfly/extension/io/IOSubsystemAdd.java
+++ io/subsystem/src/main/java/org/wildfly/extension/io/IOSubsystemAdd.java
@@ -24,12 +24,16 @@
 
 package org.wildfly.extension.io;
 
+import static org.wildfly.extension.io.IORootDefinition.IO_MAX_THREADS_RUNTIME_CAPABILITY;
+
 import org.jboss.as.controller.AbstractAddStepHandler;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.registry.Resource;
 import org.jboss.dmr.ModelNode;
-
+import org.jboss.msc.service.ServiceController;
+import org.jboss.msc.service.ServiceName;
 
 /**
  * Handler responsible for adding the subsystem resource to the model
@@ -47,5 +51,20 @@ class IOSubsystemAdd extends AbstractAddStepHandler {
     protected void performRuntime(OperationContext context, ModelNode operation, Resource resource) throws OperationFailedException {
         ModelNode workers = Resource.Tools.readModel(resource).get(IOExtension.WORKER_PATH.getKey());
         WorkerAdd.checkWorkerConfiguration(context, workers);
+
+        MaxThreadTrackerService service = new MaxThreadTrackerService();
+        ServiceName serviceName = IO_MAX_THREADS_RUNTIME_CAPABILITY.getCapabilityServiceName();
+        ServiceController<Integer> controller = context.getServiceTarget().addService(serviceName, service)
+                .setInitialMode(ServiceController.Mode.NEVER)
+                .install();
+
+        context.addStep(new OperationStepHandler() {
+            @Override
+            public void execute(OperationContext context, ModelNode operation) throws OperationFailedException {
+                controller.setMode(ServiceController.Mode.ACTIVE);
+                // Rollback handled by the parent step
+                context.completeStep(OperationContext.RollbackHandler.NOOP_ROLLBACK_HANDLER);
+            }
+        }, OperationContext.Stage.RUNTIME);
     }
 }
