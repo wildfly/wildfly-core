7eb40f7d4c38784baa89c7d4a15930e5664ea7f3
Ken Wills <ken@zaptillion.net>
2015-09-18 12:46:17 -0500

    WFCORE-981 - RemoteDomainConnection uses stale HostInfo data ServerConfigInfoImpl incorrectly uses model.has(SOCKET_BINDING_GROUP) instead of model.hasDefined(SOCKET_BINDING_GROUP)
    WFCORE-310
     - default ignore-unused-configurion to true unless --backup is used
     - update deployment test for ignored deployment on slave
     - add test for deployments and ignore-unused-configuration
     - move ignore resource tests to seperate test class, and force ignore-unused-configuration in @BeforeClass
    - ensure servers are restarted with ignore-ignore-configuration=true after IgnoredResourceTestCase is completed.

--- testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/suites/DomainTestSuite.java
+++ testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/suites/DomainTestSuite.java
@@ -43,6 +43,7 @@ import org.junit.runners.Suite;
         DirectoryGroupingByTypeTestCase.class,
         ExtensionManagementTestCase.class,
         HcExtensionAndSubsystemManagementTestCase.class,
+        IgnoreUnusedConfigurationTestCase.class,
         IgnoredResourcesTestCase.class,
         JMXHostSubsystemTestCase.class,
         ManagementAccessTestCase.class,
