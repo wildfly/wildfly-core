2176590e81d233e245bc8c012742fd7deb3e3a68
Richard Opalka <opalka.richard@gmail.com>
2015-10-23 08:57:42 +0200

    Introducing class path utility methods.

--- server/src/main/java/org/jboss/as/server/deployment/module/Utils.java
+++ server/src/main/java/org/jboss/as/server/deployment/module/Utils.java
@@ -22,11 +22,13 @@
 package org.jboss.as.server.deployment.module;
 
 import org.jboss.modules.Resource;
-import org.jboss.modules.ResourceLoader;
+import org.jboss.as.server.loaders.ResourceLoader;
 import org.jboss.vfs.VirtualFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.StringTokenizer;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
@@ -48,6 +50,113 @@ final class Utils {
         } else {
             return readManifest(resourceRoot.getRoot().getChild(JarFile.MANIFEST_NAME));
         }
+    }
+
+    static String getPathForClassPathEntry(final String canonPath, final ResourceLoader loader) {
+        if (loader == null) return null;
+        if (!canonPath.contains("../")) return canonPath;
+
+        final int upperLevelCount = canonPath.lastIndexOf("../") / 3 + 1;
+        if (upperLevelCount == 0) {
+            return canonPath;
+        } else {
+            final int loaderLevelCount = loader.getPath().split("/").length - 1;
+            if (loaderLevelCount > upperLevelCount) {
+                final String[] loaderPathItems = loader.getPath().split("/");
+                final StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < loaderPathItems.length - upperLevelCount; i++) {
+                    sb.append(loaderPathItems[i]).append("/");
+                }
+                sb.append(canonPath.substring(upperLevelCount * 3));
+                return sb.toString();
+            } else if (loaderLevelCount == 0) {
+                return getPathForClassPathEntry(canonPath.substring(3), loader.getParent());
+            } else {
+                return getPathForClassPathEntry(canonPath.substring(loaderLevelCount * 3), loader.getParent());
+            }
+        }
+    }
+
+    static ResourceLoader getLoaderForClassPathEntry(final String canonPath, final ResourceLoader loader) {
+        if (loader == null) return null;
+        if (!canonPath.contains("../")) {
+            if (loader != null && loader.getPath() != null) {
+                if (loader.getPath().equals("WEB-INF/classes")) {
+                    if (loader.getParent() != null) {
+                        return loader.getParent().getParent(); // special case
+                    }
+                }
+            }
+            return loader.getParent();
+        }
+
+        final int upperLevelCount = canonPath.lastIndexOf("../") / 3 + 1;
+        if (upperLevelCount == 0) {
+            return loader.getParent();
+        } else {
+            final int loaderLevelCount = loader.getPath().split("/").length - 1;
+            if (loaderLevelCount >= upperLevelCount || loaderLevelCount == 0) {
+                return loader.getParent();
+            } else {
+                return getLoaderForClassPathEntry(canonPath.substring(loaderLevelCount * 3), loader.getParent());
+            }
+        }
+    }
+
+    static boolean isValidClassPathEntry(final String canonPath) {
+        return !canonPath.endsWith("../") && !"".equals(canonPath);
+    }
+
+    static String getLoaderPath(final ResourceLoader loader) {
+        if (loader == null) return null;
+        ResourceLoader currentLoader = loader;
+        ResourceLoader parentLoader;
+        String fullPath = currentLoader.getPath();
+        while (currentLoader != null) {
+            parentLoader = currentLoader.getParent();
+            if (parentLoader == null || parentLoader.getPath() == null || parentLoader.getPath().equals("")) break;
+            fullPath = parentLoader.getPath() + "/" + fullPath;
+            currentLoader = parentLoader;
+        }
+        return fullPath;
+    }
+
+    static String canonicalizeClassPathEntry(final String path) {
+        if (path == null || path.equals("")) return path;
+        final StringTokenizer st = new StringTokenizer(path, "\\/" + File.separator);
+        final int length = st.countTokens();
+        final String[] pathItems = new String[length];
+        final boolean[] pathItemValid = new boolean[length];
+
+        for (int i = 0; i < length; i++) {
+            pathItems[i] = st.nextToken();
+            if (pathItems[i].equals(".")) {
+                pathItemValid[i] = false;
+                continue;
+            }
+            if (pathItems[i].equals("..") && i > 0) {
+                for (int j = i - 1; j >= 0; j--) {
+                    if (pathItemValid[j] && !pathItems[j].equals("..")) {
+                        pathItemValid[j] = false;
+                        pathItemValid[i] = false;
+                        break;
+                    } else {
+                        pathItemValid[i] = true;
+                    }
+                }
+            } else {
+                pathItemValid[i] = true;
+            }
+        }
+        final StringBuilder retVal = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            if (pathItemValid[i]) {
+                retVal.append(pathItems[i]);
+                if (i != (length - 1)) retVal.append("/");
+                if (i == (length - 1) && pathItems[i].equals(".."))  retVal.append("/");
+            }
+        }
+        return retVal.toString();
     }
 
     private static Manifest readManifest(final VirtualFile manifest) throws IOException {
