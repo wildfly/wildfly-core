24f2ae4fabe96e00ff8d0d6b1355e6746d00b467
Richard Opalka <opalka.richard@gmail.com>
2015-10-27 12:18:10 +0100

    Eliminating unused logging methods.

--- embedded/src/main/java/org/wildfly/core/embedded/logging/EmbeddedLogger.java
+++ embedded/src/main/java/org/wildfly/core/embedded/logging/EmbeddedLogger.java
@@ -34,13 +34,15 @@ import org.jboss.logging.annotations.LogMessage;
 import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
 import org.jboss.modules.ModuleLoader;
-import org.jboss.vfs.VirtualFile;
 import org.wildfly.core.embedded.EmbeddedProcessStartException;
+
+import java.lang.reflect.Method;
 
 /**
  * Date: 05.11.2011
  *
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
  */
 @MessageLogger(projectCode = "WFLYEMB", length = 4)
 public interface EmbeddedLogger extends BasicLogger {
@@ -49,86 +51,6 @@ public interface EmbeddedLogger extends BasicLogger {
      * A logger with a category of the package name.
      */
     EmbeddedLogger ROOT_LOGGER = Logger.getMessageLogger(EmbeddedLogger.class, "org.jboss.as.embedded");
-
-    /**
-     * Logs a warning message indicating the file handle, represented by the {@code file} parameter, could not be
-     * closed.
-     *
-     * @param cause the cause of the error.
-     * @param file  the file.
-     */
-    @LogMessage(level = WARN)
-    @Message(id = 1, value = "Could not close handle to mounted %s")
-    void cannotCloseFile(@Cause Throwable cause, VirtualFile file);
-
-    /**
-     * Logs a warning message indicating the class file, represented by the {@code file} parameter, could not be loaded.
-     *
-     * @param cause the cause of the error.
-     * @param file  the file that could not be loaded.
-     */
-    @LogMessage(level = WARN)
-    @Message(id = 2, value = "Could not load class file %s")
-    void cannotLoadClassFile(@Cause Throwable cause, VirtualFile file);
-
-    /**
-     * Logs a warning message indicating there was an exception closing the file.
-     *
-     * @param cause the cause of the error.
-     * @param file  the file that failed to close.
-     */
-    @LogMessage(level = WARN)
-    @Message(id = 3, value = "Exception closing file %s")
-    void errorClosingFile(@Cause Throwable cause, VirtualFile file);
-
-    /**
-     * Logs a warning message indicating there was a failure to undeploy the file.
-     *
-     * @param cause the cause of the error.
-     * @param file  the file that failed to undeploy.
-     */
-    @LogMessage(level = WARN)
-    @Message(id = 4, value = "Failed to undeploy %s")
-    void failedToUndeploy(@Cause Throwable cause, File file);
-
-    /**
-     * Logs a warning message indicating the file on the ClassPath could not be found.
-     *
-     * @param file the file that could not be found.
-     */
-    @LogMessage(level = WARN)
-    @Message(id = 5, value = "File on ClassPath could not be found: %s")
-    void fileNotFound(VirtualFile file);
-
-    /**
-     * Logs a warning message indicating an unknown file type was encountered and is being skipped.
-     *
-     * @param file the file.
-     */
-    @LogMessage(level = WARN)
-    @Message(id = 6, value = "Encountered unknown file type, skipping: %s")
-    void skippingUnknownFileType(VirtualFile file);
-
-
-    /**
-     * Creates an exception indicating the file, represented by the {@code fileName} parameter, could not be mounted.
-     */
-    @Message(id = 7, value = "Could not mount file '%s'")
-    RuntimeException cannotMountFile(@Cause Throwable cause, String fileName);
-
-    /**
-     * Creates an exception indicating the contents of the file could not be read.
-     */
-    @Message(id = 8, value = "Could not read contents of %s")
-    RuntimeException cannotReadContent(@Cause Throwable cause, VirtualFile file);
-
-    /**
-     */
-    @Message(id = 9, value = "One or more exclusion values must be specified")
-    IllegalArgumentException exclusionValuesRequired();
-
-    //@Message(id = 10, value = "WARNING: Failed to load the specified logmodule %s")
-    //String failedToLoadLogModule(ModuleIdentifier moduleId);
 
     /**
      */
@@ -142,13 +64,6 @@ public interface EmbeddedLogger extends BasicLogger {
     IllegalArgumentException invalidModulePath(String file);
 
     /**
-     * Creates an exception indicating the module, represented by the {@code moduleName} parameter, was not a valid
-     * type of {@code File[]}, {@code File}, {@code String[]} or {@code String}.
-     */
-    @Message(id = 13, value = "%s was not of type File[], File, String[] or String, but of type %s")
-    RuntimeException invalidModuleType(String moduleName, Class<?> type);
-
-    /**
      * Creates an exception indicating there was an error in the module loader.
      */
     @Message(id = 14, value = "Cannot load module %s from: %s")
@@ -159,12 +74,6 @@ public interface EmbeddedLogger extends BasicLogger {
      */
     @Message(id = 15, value = "%s is null")
     IllegalArgumentException nullVar(String name);
-
-    /**
-     * Creates an exception indicating the system property could not be found.
-     */
-    @Message(id = 16, value = "Cannot find system property: %s")
-    IllegalStateException systemPropertyNotFound(String key);
 
     @Message(id = 17, value = "Cannot load embedded server factory: %s")
     IllegalStateException cannotLoadEmbeddedServerFactory(@Cause ClassNotFoundException cause, String className);
