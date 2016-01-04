d4237ab7847f6f76a5126c19d82e04d189546831
Richard Opalka <opalka.richard@gmail.com>
2016-01-04 13:49:52 +0100

    Various fixes after rebase

--- embedded/src/main/java/org/wildfly/core/embedded/logging/EmbeddedLogger.java
+++ embedded/src/main/java/org/wildfly/core/embedded/logging/EmbeddedLogger.java
@@ -22,21 +22,15 @@
 
 package org.wildfly.core.embedded.logging;
 
-import static org.jboss.logging.Logger.Level.WARN;
-
-import java.io.File;
 import java.lang.reflect.Method;
 
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.Logger;
 import org.jboss.logging.annotations.Cause;
-import org.jboss.logging.annotations.LogMessage;
 import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
 import org.jboss.modules.ModuleLoader;
 import org.wildfly.core.embedded.EmbeddedProcessStartException;
-
-import java.lang.reflect.Method;
 
 /**
  * Date: 05.11.2011
