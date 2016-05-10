5656e2cb9c2db1f0213f38b2585c7a0633b2abe3
Kabir Khan <kkhan@redhat.com>
2016-05-10 20:49:10 +0100

    Add missing versions to the old client tests

--- testsuite/client-old-server/src/test/java/org/wildfly/client/old/server/util/Version.java
+++ testsuite/client-old-server/src/test/java/org/wildfly/client/old/server/util/Version.java
@@ -34,12 +34,16 @@ public class Version {
         AS_7_1_2_FINAL(AS, "7.1.2.Final", false, JDK.JDK6, "remote", "9999"),
         AS_7_1_3_FINAL(AS, "7.1.3.Final", false, JDK.JDK6, "remote", "9999"),
         AS_7_2_0_FINAL(AS, "7.2.0.Final", false, JDK.JDK6, "remote", "9999"),
+        WF_8_0_0_FINAL(WILDFLY, "8.0.0.Final", false, JDK.JDK8, "http-remoting", "9990"),
+        WF_8_1_0_FINAL(WILDFLY, "8.1.0.Final", false, JDK.JDK8, "http-remoting", "9990"),
+        WF_8_2_0_FINAL(WILDFLY, "8.2.0.Final", false, JDK.JDK8, "http-remoting", "9990"),
+        WF_9_0_0_FINAL(WILDFLY, "9.0.0.Final", false, JDK.JDK8, "http-remoting", "9990"),
+        WF_10_0_0_FINAL(WILDFLY, "10.0.0.Final", false, JDK.JDK8, "http-remoting", "9990"),
+        
         EAP_6_2_0(EAP, "6.2.0", true, JDK.JDK6, "remote", "9999"),
         EAP_6_3_0(EAP, "6.3.0", true, JDK.JDK7, "remote", "9999"),
         EAP_6_4_0(EAP, "6.4.0", true, JDK.JDK8, "remote", "9999"),
-        WF_8_0_0_FINAL(WILDFLY, "8.0.0.Final", false, JDK.JDK8, "http-remoting", "9990"),
-        WF_8_1_0_FINAL(WILDFLY, "8.1.0.Final", false, JDK.JDK8, "http-remoting", "9990"),
-        WF_8_2_0_FINAL(WILDFLY, "8.2.0.Final", false, JDK.JDK8, "http-remoting", "9990");
+        EAP_7_0_0(EAP, "7.0.0", true, JDK.JDK8, "http-remoting", "9990");
 
 
         private final String basename;
