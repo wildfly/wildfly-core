55f7895aa4fd8779a30f832b36b7b8d21671fbd3
Ken Wills <ken@zaptillion.net>
2016-06-28 12:29:13 -0500

    untranslate proxy translated addresses

--- controller/src/main/java/org/jboss/as/controller/ProxyOperationAddressTranslator.java
+++ controller/src/main/java/org/jboss/as/controller/ProxyOperationAddressTranslator.java
@@ -22,6 +22,7 @@
 package org.jboss.as.controller;
 
 import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
+import org.jboss.dmr.ModelNode;
 
 /**
  * Used by {@link ProxyController} implementations to translate addresses to the
@@ -32,12 +33,18 @@ import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
  */
 public interface ProxyOperationAddressTranslator {
         PathAddress translateAddress(PathAddress address);
+        PathAddress restoreAddress(PathAddress opAddress, PathAddress translatedAddress);
 
         ProxyOperationAddressTranslator NOOP = new ProxyOperationAddressTranslator() {
             @Override
             public PathAddress translateAddress(PathAddress address) {
                 return address;
             }
+
+            public PathAddress restoreAddress(PathAddress opAddress, PathAddress translatedAddress) {
+                return translatedAddress;
+            }
+
         };
 
         ProxyOperationAddressTranslator SERVER = new ProxyOperationAddressTranslator() {
@@ -46,6 +53,31 @@ public interface ProxyOperationAddressTranslator {
                 translated = lookForAndTrim(translated, ModelDescriptionConstants.HOST);
                 translated = lookForAndTrim(translated, ModelDescriptionConstants.SERVER);
                 return translated;
+            }
+
+            public PathAddress restoreAddress(PathAddress opAddress, PathAddress translatedAddress) {
+                // original op address could be /host=foo/server=bar/subsystem=*, which after translation might have a response
+                // address of /subsystem=quux. We merge the result address(es) with the original op address to restore the full
+                // address into the response, for example, /subsystem=quux becomes /host=foo/server=bar/subsystem=quux
+                ModelNode result = new ModelNode();
+                boolean found;
+                for (PathElement pe : opAddress) {
+                    found = false;
+                    if (pe.isWildcard()) {
+                        // replace the wildcards with the actual result key / values
+                        for (PathElement tpe : translatedAddress) {
+                            if (tpe.getKey().equals(pe.getKey())) {
+                                found = true;
+                                result.add(tpe.getKey(), tpe.getValue());
+                                break;
+                            }
+                        }
+                    }
+                    if (!found) {
+                        result.add(pe.getKey(), pe.getValue());
+                    }
+                }
+                return PathAddress.pathAddress(result);
             }
 
             private PathAddress lookForAndTrim(PathAddress addr, String search) {
@@ -63,5 +95,10 @@ public interface ProxyOperationAddressTranslator {
             public PathAddress translateAddress(PathAddress address) {
                 return address;
             }
+
+            public PathAddress restoreAddress(PathAddress opAddress, PathAddress translatedAddress) {
+                return translatedAddress;
+            }
+
         };
 }
