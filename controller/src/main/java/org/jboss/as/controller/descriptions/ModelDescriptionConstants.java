71f7693dfc19d9c499e3d603607dd38a61022366
Jeff Mesnil <jmesnil@gmail.com>
2016-06-30 11:27:37 +0200

    [WFCORE-1625] Fix operations output in :read-resource-description
    * in ReadResourceDescriptionHandler, delegate to
      ReadOperationDescriptionHandler's DescribedOp to ensure a consistent
      operation description in both :read-resource-description and
      :read-operation-descripion
    
    JIRA: https://issues.jboss.org/browse/WFCORE-1625

--- controller/src/main/java/org/jboss/as/controller/descriptions/ModelDescriptionConstants.java
+++ controller/src/main/java/org/jboss/as/controller/descriptions/ModelDescriptionConstants.java
@@ -52,6 +52,7 @@ public class ModelDescriptionConstants {
     public static final String ALLOW_RESOURCE_SERVICE_RESTART = "allow-resource-service-restart";
     public static final String ALLOWED_ORIGINS = "allowed-origins";
     public static final String ALL_ROLE_NAMES = "all-role-names";
+    public static final String ALL_SERVICES = "all-services";
     public static final String ALTERNATIVES = "alternatives";
     public static final String ANY = "any";
     public static final String ANY_ADDRESS = "any-address";
@@ -298,6 +299,7 @@ public class ModelDescriptionConstants {
     public static final String NETWORK = "network";
     public static final String NILLABLE = "nillable";
     public static final String NIL_SIGNIFICANT = "nil-significant";
+    public static final String NO_SERVICES = "no-services";
     public static final String NOT = "not";
     public static final String NOTIFICATION = "notification";
     public static final String NOTIFICATION_DATA_TYPE = "data-type";
@@ -385,6 +387,7 @@ public class ModelDescriptionConstants {
     public static final String RESOLVE_EXPRESSIONS = "resolve-expressions";
     public static final String RESOURCE_ADDED_NOTIFICATION = "resource-added";
     public static final String RESOURCE_REMOVED_NOTIFICATION = "resource-removed";
+    public static final String RESOURCE_SERVICES = "resource-services";
     public static final String RESPONSE = "response";
     public static final String RESPONSE_HEADERS = "response-headers";
     public static final String RESTART = "restart";
