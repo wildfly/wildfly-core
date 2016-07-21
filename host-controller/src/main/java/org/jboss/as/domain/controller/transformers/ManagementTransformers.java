72306a42a7ac311710d6d0174d2605c5cc7bca06
Tomaz Cerar <tomaz.cerar@gmail.com>
2016-07-21 17:59:24 +0200

    WFCORE-1669 SERVER_SSL constraint is not modifiable in a mixed domain

--- host-controller/src/main/java/org/jboss/as/domain/controller/transformers/ManagementTransformers.java
+++ host-controller/src/main/java/org/jboss/as/domain/controller/transformers/ManagementTransformers.java
@@ -22,31 +22,23 @@
 
 package org.jboss.as.domain.controller.transformers;
 
-import java.util.Locale;
-
-import org.jboss.as.controller.ExpressionResolver;
 import org.jboss.as.controller.ModelVersion;
-import org.jboss.as.controller.OperationFailedException;
-import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.PathElement;
-import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
-import org.jboss.as.controller.registry.Resource;
-import org.jboss.as.controller.transform.ResourceTransformationContext;
-import org.jboss.as.controller.transform.ResourceTransformer;
-import org.jboss.as.controller.transform.TransformationContext;
+import org.jboss.as.controller.access.constraint.SensitivityClassification;
 import org.jboss.as.controller.transform.description.ChainedTransformationDescriptionBuilder;
-import org.jboss.as.controller.transform.description.DiscardAttributeChecker;
-import org.jboss.as.controller.transform.description.RejectAttributeChecker;
 import org.jboss.as.controller.transform.description.ResourceTransformationDescriptionBuilder;
 import org.jboss.as.controller.transform.description.TransformationDescriptionBuilder;
 import org.jboss.as.domain.management.CoreManagementResourceDefinition;
 import org.jboss.as.domain.management.access.AccessAuthorizationResourceDefinition;
-import org.jboss.dmr.ModelNode;
+import org.jboss.as.domain.management.access.AccessConstraintResources;
+import org.jboss.as.domain.management.access.SensitivityClassificationTypeResourceDefinition;
+import org.jboss.as.domain.management.access.SensitivityResourceDefinition;
 
 /**
  * Transformers for the domain-wide management configuration.
  *
  * @author Brian Stansberry (c) 2013 Red Hat Inc.
+ * @author Tomaz Cerar
  */
 class ManagementTransformers {
 
@@ -58,41 +50,13 @@ class ManagementTransformers {
         // Discard the domain level core-service=management resource and its children unless RBAC is enabled
         // Configuring rbac details is OK (i.e. discarable), so long as the provider is not enabled
         ChainedTransformationDescriptionBuilder chainedBuilder = TransformationDescriptionBuilder.Factory.createChainedInstance(CoreManagementResourceDefinition.PATH_ELEMENT, currentVersion);
-        ResourceTransformationDescriptionBuilder builder = chainedBuilder.createBuilder(currentVersion, DomainTransformers.VERSION_1_8);
-        builder.setCustomResourceTransformer(new ResourceTransformer() {
-            @Override
-            public void transformResource(ResourceTransformationContext context, PathAddress address, Resource resource) throws OperationFailedException {
-                Resource accessAuth = resource.getChild(AccessAuthorizationResourceDefinition.PATH_ELEMENT);
-                if (accessAuth != null) {
-                    ModelNode model = accessAuth.getModel();
-                    String providerName = AccessAuthorizationResourceDefinition.PROVIDER.resolveModelAttribute(ExpressionResolver.REJECTING, model).asString();
-                    AccessAuthorizationResourceDefinition.Provider provider = AccessAuthorizationResourceDefinition.Provider.valueOf(providerName.toUpperCase(Locale.ENGLISH));
-                    if (provider != AccessAuthorizationResourceDefinition.Provider.SIMPLE) {
-                        // Continue to lower levels where this will be rejected
-                        final ResourceTransformationContext childContext = context.addTransformedResource(PathAddress.EMPTY_ADDRESS, resource);
-                        childContext.processChildren(resource);
-                    }
-                }
 
-                // else do nothing, i.e. acts as a discard
-            }
-        });
-        ResourceTransformationDescriptionBuilder accessBuilder = builder.addChildResource(AccessAuthorizationResourceDefinition.PATH_ELEMENT)
-            .getAttributeBuilder()
-                .addRejectCheck(RejectAttributeChecker.DEFINED, AccessAuthorizationResourceDefinition.PROVIDER)
-                .setDiscard(new DiscardAttributeChecker.DefaultDiscardAttributeChecker() {
-                    @Override
-                    protected boolean isValueDiscardable(PathAddress address, String attributeName, ModelNode attributeValue, TransformationContext context) {
-                        return !attributeValue.isDefined()
-                                || AccessAuthorizationResourceDefinition.Provider.SIMPLE.toString().equalsIgnoreCase(attributeValue.asString());
-                    }
-                }, AccessAuthorizationResourceDefinition.PROVIDER)
-                .setDiscard(DiscardAttributeChecker.ALWAYS, AccessAuthorizationResourceDefinition.PERMISSION_COMBINATION_POLICY)
-                .end();
-        accessBuilder.discardChildResource(PathElement.pathElement(ModelDescriptionConstants.CONSTRAINT));
-        accessBuilder.discardChildResource(PathElement.pathElement(ModelDescriptionConstants.ROLE_MAPPING));
-        accessBuilder.discardChildResource(PathElement.pathElement(ModelDescriptionConstants.SERVER_GROUP_SCOPED_ROLE));
-        accessBuilder.discardChildResource(PathElement.pathElement(ModelDescriptionConstants.HOST_SCOPED_ROLE));
+        ResourceTransformationDescriptionBuilder builderCurrentTo17 = chainedBuilder.createBuilder(currentVersion, DomainTransformers.VERSION_1_7);
+        builderCurrentTo17.addChildResource(AccessAuthorizationResourceDefinition.PATH_ELEMENT)
+                .addChildResource(AccessConstraintResources.SENSITIVITY_PATH_ELEMENT)
+                .addChildResource(SensitivityClassificationTypeResourceDefinition.PATH_ELEMENT)
+                .discardChildResource(PathElement.pathElement(SensitivityResourceDefinition.PATH_ELEMENT.getKey(), SensitivityClassification.SERVER_SSL.getName()))
+                .build();
 
         return chainedBuilder;
     }
