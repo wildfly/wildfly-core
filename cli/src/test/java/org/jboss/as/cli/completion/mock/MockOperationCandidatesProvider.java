5ca5693ce88d8563810ae0363196b45b856901a0
James Perkins <jperkins@redhat.com>
2016-08-03 07:43:30 -0700

    Revert "WFCORE-1487 Include indexed properties in the suggestion list if no completer exists"
    This reverts commit c12008ee7c16331a610859ee828a58118c91ccaf.

--- cli/src/test/java/org/jboss/as/cli/completion/mock/MockOperationCandidatesProvider.java
+++ cli/src/test/java/org/jboss/as/cli/completion/mock/MockOperationCandidatesProvider.java
@@ -114,13 +114,13 @@ public class MockOperationCandidatesProvider implements OperationCandidatesProvi
             return Collections.emptyList();
         }
 
-        final List<MockOperation.Property> properties = operation.getProperties();
-        final List<CommandArgument> result = new ArrayList<CommandArgument>(properties.size());
-        for(final MockOperation.Property p : properties) {
+        final List<String> names = operation.getPropertyNames();
+        final List<CommandArgument> result = new ArrayList<CommandArgument>(names.size());
+        for(final String name : names) {
             result.add(new CommandArgument(){
                 @Override
                 public String getFullName() {
-                    return p.getName();
+                    return name;
                 }
 
                 @Override
@@ -130,13 +130,13 @@ public class MockOperationCandidatesProvider implements OperationCandidatesProvi
 
                 @Override
                 public int getIndex() {
-                    return p.getIndex();
+                    return -1;
                 }
 
                 @Override
                 public boolean isPresent(ParsedCommandLine args)
                         throws CommandFormatException {
-                    return args.hasProperty(p.getName());
+                    return args.hasProperty(name);
                 }
 
                 @Override
@@ -150,15 +150,15 @@ public class MockOperationCandidatesProvider implements OperationCandidatesProvi
 
                 @Override
                 public String getValue(ParsedCommandLine args) throws CommandFormatException {
-                    return args.getPropertyValue(p.getName());
+                    return args.getPropertyValue(name);
                 }
 
                 @Override
                 public String getValue(ParsedCommandLine args, boolean required) throws CommandFormatException {
                     if(!isPresent(args)) {
-                        throw new CommandFormatException("Property '" + p.getName() + "' is missing required value.");
+                        throw new CommandFormatException("Property '" + name + "' is missing required value.");
                     }
-                    return args.getPropertyValue(p.getName());
+                    return args.getPropertyValue(name);
                 }
 
                 @Override
@@ -166,7 +166,7 @@ public class MockOperationCandidatesProvider implements OperationCandidatesProvi
                     if(!isPresent(args)) {
                         return false;
                     }
-                    if(p.getName().equals(args.getLastParsedPropertyName())) {
+                    if(name.equals(args.getLastParsedPropertyName())) {
                         return false;
                     }
                     return true;
