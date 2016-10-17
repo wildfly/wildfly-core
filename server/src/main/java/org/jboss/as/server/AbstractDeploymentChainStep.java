d57a51782e742284d592ee2678874438ee0fd94a
Brian Stansberry <brian.stansberry@redhat.com>
2016-10-17 17:31:02 -0500

    [WFCORE-1781] AbstractDeploymentChainStep should do some state validation before modifying DUPs

--- server/src/main/java/org/jboss/as/server/AbstractDeploymentChainStep.java
+++ server/src/main/java/org/jboss/as/server/AbstractDeploymentChainStep.java
@@ -22,8 +22,15 @@
 
 package org.jboss.as.server;
 
+import java.util.Collections;
+import java.util.Set;
+
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationStepHandler;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
+import org.jboss.as.controller.registry.ImmutableManagementResourceRegistration;
+import org.jboss.as.controller.registry.OperationEntry;
 import org.jboss.as.server.deployment.DeploymentUnitProcessor;
 import org.jboss.as.server.deployment.Phase;
 import org.jboss.dmr.ModelNode;
@@ -47,7 +54,26 @@ public abstract class AbstractDeploymentChainStep implements OperationStepHandle
     };
 
     public final void execute(final OperationContext context, final ModelNode operation) {
-        execute(TARGET);
+        if (context.isBooting()) {
+            execute(TARGET);
+        } else {
+            // WFCORE-1781 We should not be called post-boot as the DUP chain can only be modified in boot
+            // Check and see if the OperationDefinition for this op declares reload/restart required and if so
+            // trigger that; otherwise fail.
+            ImmutableManagementResourceRegistration mrr = context.getResourceRegistration();
+            OperationEntry oe = mrr.getOperationEntry(PathAddress.EMPTY_ADDRESS, operation.get(ModelDescriptionConstants.OP).asString());
+            Set<OperationEntry.Flag> flags = oe == null ? Collections.emptySet() : oe.getOperationDefinition().getFlags();
+            if (flags.contains(OperationEntry.Flag.RESTART_JVM)) {
+                context.restartRequired();
+                context.completeStep((ctx, op) -> ctx.revertRestartRequired());
+            } else if (flags.contains(OperationEntry.Flag.RESTART_ALL_SERVICES)) {
+                context.reloadRequired();
+                context.completeStep(OperationContext.RollbackHandler.REVERT_RELOAD_REQUIRED_ROLLBACK_HANDLER);
+            } else {
+                // Coding error we cannot recover from
+                throw new IllegalStateException();
+            }
+        }
     }
 
     /**
