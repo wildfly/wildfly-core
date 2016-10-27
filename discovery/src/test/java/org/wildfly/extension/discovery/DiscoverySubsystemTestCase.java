0c422ea6f8a41ad8a35f8bc795d62f0dbf110b35
Tomaz Cerar <tomaz.cerar@gmail.com>
2016-10-27 15:54:42 +0200

    [WFCORE-1884] Add subsystem test

---
+++ discovery/src/test/java/org/wildfly/extension/discovery/DiscoverySubsystemTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016, Red Hat, Inc., and individual contributors as indicated
+ * by the @authors tag.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.extension.discovery;
+
+import java.io.IOException;
+
+import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
+
+/**
+ * @author <a href="mailto:tomaz.cerar@redhat.com">Tomaz Cerar</a>
+ */
+public class DiscoverySubsystemTestCase extends AbstractSubsystemBaseTest {
+
+    public DiscoverySubsystemTestCase() {
+        super(DiscoveryExtension.SUBSYSTEM_NAME, new DiscoveryExtension());
+    }
+
+
+    @Override
+    protected String getSubsystemXml() throws IOException {
+        return readResource("discovery-1.0.xml");
+    }
+
+    @Override
+    protected String getSubsystemXsdPath() throws Exception {
+        return "schema/wildfly-discovery_1_0.xsd";
+    }
+
+    @Override
+    protected String[] getSubsystemTemplatePaths() throws IOException {
+        return new String[]{
+                "/subsystem-templates/discovery.xml"
+        };
+    }
+
+}
