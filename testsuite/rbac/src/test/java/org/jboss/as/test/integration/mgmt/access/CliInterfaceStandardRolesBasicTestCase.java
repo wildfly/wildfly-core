c85706700a8a506f392dcaecd3b82334f6877874
Darran Lofthouse <darran.lofthouse@jboss.com>
2016-11-02 18:35:26 +0000

    [WFCORE-673] Ensure the Elytron provider is registered before the test runs.

--- testsuite/rbac/src/test/java/org/jboss/as/test/integration/mgmt/access/CliInterfaceStandardRolesBasicTestCase.java
+++ testsuite/rbac/src/test/java/org/jboss/as/test/integration/mgmt/access/CliInterfaceStandardRolesBasicTestCase.java
@@ -23,11 +23,16 @@
 package org.jboss.as.test.integration.mgmt.access;
 
 import org.jboss.as.test.integration.management.rbac.RbacAdminCallbackHandler;
+
+import java.security.Security;
+
 import org.jboss.as.test.integration.management.interfaces.CliManagementInterface;
 import org.jboss.as.test.integration.management.interfaces.ManagementInterface;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.wildfly.core.testrunner.ServerSetup;
 import org.wildfly.core.testrunner.WildflyTestRunner;
+import org.wildfly.security.WildFlyElytronProvider;
 
 /**
  * @author jcechace
@@ -36,6 +41,12 @@ import org.wildfly.core.testrunner.WildflyTestRunner;
 @RunWith(WildflyTestRunner.class)
 @ServerSetup({StandardUsersSetupTask.class, StandardExtensionSetupTask.class})
 public class CliInterfaceStandardRolesBasicTestCase extends StandardRolesBasicTestCase {
+
+    @BeforeClass
+    public static void installProvider() {
+        Security.insertProviderAt(new WildFlyElytronProvider(), 0);
+    }
+
     @Override
     protected ManagementInterface createClient(String userName) {
         return CliManagementInterface.create(
