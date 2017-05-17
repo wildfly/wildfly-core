2e40fb5dd3739433bc6d71bf5f8739ef4cc82c14
Tomaz Cerar <tomaz.cerar@gmail.com>
2017-05-17 14:41:21 +0200

    transformers cleanup

--- subsystem-test/framework/src/main/java/org/jboss/as/subsystem/test/TransformationUtils.java
+++ subsystem-test/framework/src/main/java/org/jboss/as/subsystem/test/TransformationUtils.java
@@ -22,22 +22,30 @@
 
 package org.jboss.as.subsystem.test;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.jboss.as.controller.logging.ControllerLogger;
 import org.jboss.as.controller.ModelVersion;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.ResourceDefinition;
+import org.jboss.as.controller.logging.ControllerLogger;
 import org.jboss.as.controller.registry.AttributeAccess;
 import org.jboss.as.controller.registry.ImmutableManagementResourceRegistration;
 import org.jboss.as.controller.registry.LegacyResourceDefinition;
 import org.jboss.as.controller.registry.Resource;
 import org.jboss.dmr.ModelNode;
 import org.jboss.dmr.Property;
+import org.junit.Assert;
 
 /**
  * @author <a href="mailto:tomaz.cerar@redhat.com">Tomaz Cerar</a>
@@ -48,7 +56,7 @@ class TransformationUtils {
         //
     }
 
-    public static ModelNode getSubsystemDefinitionForVersion(final Class<?> classForDmrPackage, final String subsystemName, ModelVersion version) {
+    private static ModelNode getSubsystemDefinitionForVersion(final Class<?> classForDmrPackage, final String subsystemName, ModelVersion version) {
 
         StringBuilder key = new StringBuilder(subsystemName).append("-").append(version.getMajor()).append(".").append(version.getMinor());
         key.append('.').append(version.getMicro()).append(".dmr");
@@ -73,7 +81,7 @@ class TransformationUtils {
         return null;
     }
 
-    public static ResourceDefinition loadSubsystemDefinitionFromFile(final Class<?> classForDmrPackage, final String subsystemName, ModelVersion version) {
+    private static ResourceDefinition loadSubsystemDefinitionFromFile(final Class<?> classForDmrPackage, final String subsystemName, ModelVersion version) {
         final ModelNode desc = getSubsystemDefinitionForVersion(classForDmrPackage, subsystemName, version);
         if (desc == null) {
             return null;
@@ -81,7 +89,7 @@ class TransformationUtils {
         return new LegacyResourceDefinition(desc);
     }
 
-    public static Resource modelToResource(final PathAddress startAddress, final ImmutableManagementResourceRegistration reg, final ModelNode model, boolean includeUndefined) {
+    private static Resource modelToResource(final PathAddress startAddress, final ImmutableManagementResourceRegistration reg, final ModelNode model, boolean includeUndefined) {
         return modelToResource(startAddress, reg, model, includeUndefined, PathAddress.EMPTY_ADDRESS);
     }
 
@@ -137,8 +145,58 @@ class TransformationUtils {
         return res;
     }
 
-    public static Resource modelToResource(final ImmutableManagementResourceRegistration reg, final ModelNode model, boolean includeUndefined) {
+    static Resource modelToResource(final ImmutableManagementResourceRegistration reg, final ModelNode model, boolean includeUndefined) {
         return TransformationUtils.modelToResource(PathAddress.EMPTY_ADDRESS, reg, model, includeUndefined);
+    }
+
+    /**
+     * Dumps the target subsystem resource description to DMR format, needed by TransformerRegistry for non-standard subsystems
+     *
+     * @param kernelServices the kernel services for the started controller
+     * @param modelVersion   the target subsystem model version
+     * @param mainSubsystemName name of subsystem
+     * @param dmrFile file where to write description
+     */
+    private static void generateLegacySubsystemResourceRegistrationDmr(KernelServices kernelServices, ModelVersion modelVersion, String mainSubsystemName, Path dmrFile) throws IOException {
+        KernelServices legacy = kernelServices.getLegacyServices(modelVersion);
+
+        //Generate the org.jboss.as.controller.transform.subsystem-version.dmr file - just use the format used by TransformerRegistry for now
+        PathAddress pathAddress = PathAddress.pathAddress(PathElement.pathElement(SUBSYSTEM, mainSubsystemName));
+        ModelNode desc = ((KernelServicesInternal) legacy).readFullModelDescription(pathAddress.toModelNode());
+        try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(dmrFile, StandardCharsets.UTF_8))) {
+            desc.writeString(pw, false);
+            //System.out.println("Legacy resource definition dmr written to: " + dmrFile.getAbsolutePath());
+        }
+    }
+
+    static ResourceDefinition getResourceDefinition(KernelServices kernelServices, ModelVersion modelVersion, String mainSubsystemName) throws IOException {
+        //Look for the file in the org.jboss.as.subsystem.test package - this is where we used to store them before the split
+        ResourceDefinition rd = TransformationUtils.loadSubsystemDefinitionFromFile(TransformationUtils.class.getClass(), mainSubsystemName, modelVersion);
+
+        if (rd == null) {
+            //This is the 'new' post-split way. First check for a cached .dmr file. This which also allows people
+            //to override the file for the very rare cases where the rd needed touching up (probably only the case for 7.1.x descriptions).
+            File file = getDmrFile(kernelServices, modelVersion, mainSubsystemName);
+            if (!file.exists()) {
+                generateLegacySubsystemResourceRegistrationDmr(kernelServices, modelVersion, mainSubsystemName, file.toPath());
+            }
+            //System.out.println("Using legacy resource definition dmr: " + file);
+            rd = TransformationUtils.loadSubsystemDefinitionFromFile(kernelServices.getTestClass(), mainSubsystemName, modelVersion);
+        }
+        return rd;
+    }
+
+    private static File getDmrFile(KernelServices kernelServices, ModelVersion modelVersion, String mainSubsystemName) {
+
+        File file = new File("target/test-classes").getAbsoluteFile();
+        Assert.assertTrue(file.exists());
+        for (String part : kernelServices.getTestClass().getPackage().getName().split("\\.")) {
+            file = new File(file, part);
+            if (!file.exists()) {
+                file.mkdir();
+            }
+        }
+        return new File(file, mainSubsystemName + "-" + modelVersion.getMajor() + "." + modelVersion.getMinor() + "." + modelVersion.getMicro() + ".dmr");
     }
 
 }
