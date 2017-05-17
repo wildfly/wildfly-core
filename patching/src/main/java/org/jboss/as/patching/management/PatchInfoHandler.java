f02c2bfd26bfbda2ec2a9cfa86cf252afea82488
Brian Stansberry <brian.stansberry@redhat.com>
2017-05-17 11:50:36 -0500

    [WFCORE-2823] Do runtime patching work in Stage.RUNTIME
    One use case for the patch-info op does read-resource stuff that has to run in MODEL, so split that out from the common pattern used for everything else

--- patching/src/main/java/org/jboss/as/patching/management/PatchInfoHandler.java
+++ patching/src/main/java/org/jboss/as/patching/management/PatchInfoHandler.java
@@ -22,8 +22,13 @@
 
 package org.jboss.as.patching.management;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.INCLUDE_RUNTIME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.READ_RESOURCE_OPERATION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RECURSIVE;
+
 import org.jboss.as.controller.OperationContext;
-import org.jboss.as.controller.OperationContext.Stage;
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.PathAddress;
@@ -48,39 +53,48 @@ public class PatchInfoHandler extends PatchStreamResourceOperationStepHandler {
     public static final PatchInfoHandler INSTANCE = new PatchInfoHandler();
 
     @Override
+    public void execute(OperationContext context, ModelNode operation) throws OperationFailedException {
+        String patchId = PatchResourceDefinition.PATCH_ID_OPTIONAL.resolveModelAttribute(context, operation).asStringOrNull();
+
+        if (patchId != null) {
+            super.execute(context, operation);
+        } else {
+            final ModelNode readResource = new ModelNode();
+            readResource.get(OP_ADDR).set(operation.get(OP_ADDR));
+            readResource.get(OP).set(READ_RESOURCE_OPERATION);
+            readResource.get(RECURSIVE).set(true);
+            readResource.get(INCLUDE_RUNTIME).set(true);
+            final OperationStepHandler readResHandler = context.getRootResourceRegistration().getOperationHandler(PathAddress.EMPTY_ADDRESS, READ_RESOURCE_OPERATION);
+            context.addStep(readResource, readResHandler, OperationContext.Stage.MODEL);
+        }
+    }
+
+    @Override
     protected void execute(final OperationContext context, final ModelNode operation, final InstalledIdentity installedIdentity) throws OperationFailedException {
 
-        final ModelNode patchIdNode = PatchResourceDefinition.PATCH_ID_OPTIONAL.resolveModelAttribute(context, operation);
-        final String patchId = patchIdNode.isDefined() ? patchIdNode.asString() : null;
+        String patchId = PatchResourceDefinition.PATCH_ID_OPTIONAL.resolveModelAttribute(context, operation).asStringOrNull();
+        assert patchId != null;  // the overridden execute(OperationContext context, ModelNode operation) ensures not
 
-        if(patchId == null) {
-            final ModelNode readResource = new ModelNode();
-            readResource.get("address").set(operation.get("address"));
-            readResource.get("operation").set("read-resource");
-            readResource.get("recursive").set(true);
-            readResource.get("include-runtime").set(true);
-            final OperationStepHandler readResHandler = context.getRootResourceRegistration().getOperationHandler(PathAddress.EMPTY_ADDRESS, "read-resource");
-            context.addStep(readResource, readResHandler, Stage.MODEL);
+        final boolean verbose = PatchResourceDefinition.VERBOSE.resolveModelAttribute(context, operation).asBoolean();
+        final PatchableTarget.TargetInfo info;
+        try {
+            info = installedIdentity.getIdentity().loadTargetInfo();
+        } catch (Exception e) {
+            throw new OperationFailedException(PatchLogger.ROOT_LOGGER.failedToLoadInfo(installedIdentity.getIdentity().getName()), e);
+        }
+
+        final PatchingHistory.Iterator i = PatchingHistory.Factory.iterator(installedIdentity, info);
+        final ModelNode result = patchIdInfo(context, patchId, verbose, i);
+        if (result == null) {
+            context.getFailureDescription().set(PatchLogger.ROOT_LOGGER.patchNotFoundInHistory(patchId).getLocalizedMessage());
         } else {
-            final boolean verbose = PatchResourceDefinition.VERBOSE.resolveModelAttribute(context, operation).asBoolean();
-            final PatchableTarget.TargetInfo info;
-            try {
-                info = installedIdentity.getIdentity().loadTargetInfo();
-            } catch (Exception e) {
-                throw new OperationFailedException(PatchLogger.ROOT_LOGGER.failedToLoadInfo(installedIdentity.getIdentity().getName()), e);
-            }
-
-            final PatchingHistory.Iterator i = PatchingHistory.Factory.iterator(installedIdentity, info);
-            final ModelNode result = patchIdInfo(context, patchId, verbose, i);
-            if (result == null) {
-                context.getFailureDescription().set(PatchLogger.ROOT_LOGGER.patchNotFoundInHistory(patchId).getLocalizedMessage());
-            }
             context.getResult().set(result);
         }
+
         context.completeStep(OperationContext.RollbackHandler.NOOP_ROLLBACK_HANDLER);
     }
 
-    protected ModelNode patchIdInfo(final OperationContext context, final String patchId, final boolean verbose, final PatchingHistory.Iterator i) {
+    private ModelNode patchIdInfo(final OperationContext context, final String patchId, final boolean verbose, final PatchingHistory.Iterator i) {
         while(i.hasNext()) {
             final Entry entry = i.next();
             if(patchId.equals(entry.getPatchId())) {
