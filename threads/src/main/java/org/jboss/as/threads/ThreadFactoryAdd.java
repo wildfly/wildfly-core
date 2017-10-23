4027dab85f5cd491d71b287071ffeb1bc95327b0
Jeff Mesnil <jmesnil@gmail.com>
2017-10-23 11:58:59 +0200

    [WFLY-3377] thread-factory resource creation
    * do not use the values of group-name and thread-name-pattern if the
      attibutes are not defined in the model.
    
    JIRA: https://issues.jboss.org/browse/WFCORE-3377

--- threads/src/main/java/org/jboss/as/threads/ThreadFactoryAdd.java
+++ threads/src/main/java/org/jboss/as/threads/ThreadFactoryAdd.java
@@ -55,10 +55,12 @@ public class ThreadFactoryAdd extends AbstractAddStepHandler {
     protected void performRuntime(final OperationContext context, final ModelNode operation, final ModelNode model) throws OperationFailedException {
 
         ModelNode priorityModelNode = PoolAttributeDefinitions.PRIORITY.resolveModelAttribute(context, model);
+        ModelNode groupNameModelNode = PoolAttributeDefinitions.GROUP_NAME.resolveModelAttribute(context, model);
+        ModelNode threadNamePatternModelNode = PoolAttributeDefinitions.THREAD_NAME_PATTERN.resolveModelAttribute(context, model);
 
-        final String threadNamePattern = PoolAttributeDefinitions.THREAD_NAME_PATTERN.resolveModelAttribute(context, model).asString();
+        final String threadNamePattern = threadNamePatternModelNode.isDefined() ? threadNamePatternModelNode.asString() : null;
         final Integer priority = priorityModelNode.isDefined() ? new Integer(priorityModelNode.asInt()) : null;
-        final String groupName = PoolAttributeDefinitions.GROUP_NAME.resolveModelAttribute(context, model).asString();
+        final String groupName = groupNameModelNode.isDefined() ? groupNameModelNode.asString() : null;
 
         final String name = context.getCurrentAddressValue();
 
