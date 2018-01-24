86d4aecc1896c70bf496c6701762556183299ffb
Jean-Francois Denise <jfdenise@gmail.com>
2018-01-24 16:43:52 +0100

    Upgrade to aesh-1.0-Alpha5, added unit test to cover >> operator behavior change

--- cli/src/test/java/org/jboss/as/cli/OperatorsTestCase.java
+++ cli/src/test/java/org/jboss/as/cli/OperatorsTestCase.java
@@ -135,6 +135,41 @@ public class OperatorsTestCase {
     }
 
     @Test
+    public void testAppendOutCreateFile() throws CliInitializationException, CommandLineException, IOException {
+        ByteArrayOutputStream consoleOutput = new ByteArrayOutputStream();
+        CommandContextConfiguration config = new CommandContextConfiguration.Builder().setConsoleOutput(consoleOutput).build();
+        CommandContext ctx = CommandContextFactory.getInstance().newCommandContext(config);
+        File useless = File.createTempFile("useless" + System.currentTimeMillis(), null);
+        File actualFile = new File(useless.getParent(), "cli_test" + System.currentTimeMillis());
+        try {
+            ctx.handle("version >> " + actualFile.getAbsolutePath());
+            String out = consoleOutput.toString();
+            Assert.assertTrue(out, out.isEmpty());
+            List<String> lines = Files.readAllLines(actualFile.toPath());
+            boolean found = false;
+            for (String s : lines) {
+                if (s.contains("Release:")) {
+                    found = true;
+                }
+            }
+            if (!found) {
+                throw new RuntimeException("Not right content in " + actualFile + ": " + lines);
+            }
+            consoleOutput.reset();
+            ctx.handle("grep Rel " + actualFile.getAbsolutePath());
+            out = consoleOutput.toString();
+            Assert.assertTrue(out, out.contains("Release:"));
+            ctx.handle("version >> " + actualFile.getAbsolutePath());
+            List<String> lines2 = Files.readAllLines(actualFile.toPath());
+            Assert.assertEquals(lines.size() * 2, lines2.size());
+        } finally {
+            useless.delete();
+            actualFile.delete();
+            ctx.terminateSession();
+        }
+    }
+
+    @Test
     public void testVariables() throws CliInitializationException, CommandLineException, IOException {
         ByteArrayOutputStream consoleOutput = new ByteArrayOutputStream();
         CommandContextConfiguration config = new CommandContextConfiguration.Builder().setConsoleOutput(consoleOutput).build();
