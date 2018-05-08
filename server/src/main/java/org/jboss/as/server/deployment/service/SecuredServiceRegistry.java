2192285883baf238db04efb528ab6f3ce955ce2d
Richard Opálka <opalka.richard@gmail.com>
2018-05-08 12:45:49 +0200

    [WFCORE-3809] Refactoring - use Delegating MSC classes everywhere.

--- server/src/main/java/org/jboss/as/server/deployment/service/SecuredServiceRegistry.java
+++ server/src/main/java/org/jboss/as/server/deployment/service/SecuredServiceRegistry.java
@@ -1,54 +1,35 @@
 package org.jboss.as.server.deployment.service;
 
 import org.jboss.as.server.security.ServerPermission;
-import org.jboss.msc.service.ServiceController;
-import org.jboss.msc.service.ServiceName;
-import org.jboss.msc.service.ServiceNotFoundException;
+import org.jboss.msc.service.DelegatingServiceRegistry;
 import org.jboss.msc.service.ServiceRegistry;
 
 import java.security.Permission;
-import java.util.List;
 
 /**
- *
  * TODO: these checks should be part of MSC
  * @author Stuart Douglas
+ * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
  */
-public class SecuredServiceRegistry implements ServiceRegistry {
+public class SecuredServiceRegistry extends DelegatingServiceRegistry {
 
     /**
-     * A {@link org.jboss.as.server.security.ServerPermission} needed to use {@link org.jboss.as.server.deployment.service.SecuredServiceRegistry}, i.e. invoke its methods. The name of the permission is "{@code useServiceRegistry}."
+     * A {@link org.jboss.as.server.security.ServerPermission} needed to use
+     * {@link org.jboss.as.server.deployment.service.SecuredServiceRegistry}, i.e. invoke its methods.
+     * The name of the permission is "{@code useServiceRegistry}."
      */
-    public static final Permission PERMISSION = ServerPermission.USE_SERVICE_REGISTRY;
+    private static final Permission PERMISSION = ServerPermission.USE_SERVICE_REGISTRY;
 
-    private final ServiceRegistry delegate;
-
-    public SecuredServiceRegistry(ServiceRegistry delegate) {
-        this.delegate = delegate;
+    public SecuredServiceRegistry(final ServiceRegistry delegate) {
+        super(delegate);
     }
 
     @Override
-    public ServiceController<?> getRequiredService(ServiceName serviceName) throws ServiceNotFoundException {
-        checkPermission(PERMISSION);
-        return delegate.getRequiredService(serviceName);
-    }
-
-    @Override
-    public ServiceController<?> getService(ServiceName serviceName) {
-        checkPermission(PERMISSION);
-        return delegate.getService(serviceName);
-    }
-
-    @Override
-    public List<ServiceName> getServiceNames() {
-        checkPermission(PERMISSION);
-        return delegate.getServiceNames();
-    }
-
-    private static void checkPermission(final Permission permission) {
-        SecurityManager securityManager = System.getSecurityManager();
+    protected ServiceRegistry getDelegate() {
+        final SecurityManager securityManager = System.getSecurityManager();
         if (securityManager != null) {
-            securityManager.checkPermission(permission);
+            securityManager.checkPermission(PERMISSION);
         }
+        return super.getDelegate();
     }
 }
