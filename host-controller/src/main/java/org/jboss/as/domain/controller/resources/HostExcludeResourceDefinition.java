c0d500830b7ed02619994af783a93a80d0733e09
Brian Stansberry <brian.stansberry@redhat.com>
2018-08-06 16:10:44 -0500

    [WFCORE-3994] Add WildFly 13 as a known release for use in host-exclude configs

--- host-controller/src/main/java/org/jboss/as/domain/controller/resources/HostExcludeResourceDefinition.java
+++ host-controller/src/main/java/org/jboss/as/domain/controller/resources/HostExcludeResourceDefinition.java
@@ -64,7 +64,8 @@ public class HostExcludeResourceDefinition extends SimpleResourceDefinition {
         WILDFLY10("WildFly10.0", 4, 0),
         WILDFLY10_1("WildFly10.1", 4, 2),
         WILDFLY11("WildFly11.0", 5, 0),
-        WILDFLY12("WildFly12.0", 6, 0);
+        WILDFLY12("WildFly12.0", 6, 0),
+        WILDFLY13("WildFly13.0", 7, 0);
 
         private static final Map<String, KnownRelease> map = new HashMap<>();
         static {
