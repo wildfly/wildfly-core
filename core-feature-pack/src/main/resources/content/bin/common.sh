142602170743f7d4fd45b34e45b39aaab61b585b
Richard Opálka <opalka.richard@gmail.com>
2018-08-30 15:09:40 +0200

    [WFLY-3962] Fixing common.sh script - pass all function arguments to grep command.

--- core-feature-pack/src/main/resources/content/bin/common.sh
+++ core-feature-pack/src/main/resources/content/bin/common.sh
@@ -3,7 +3,7 @@
 setDefaultModularJvmOptions() {
   $JAVA --add-modules=java.se -version > /dev/null 2>&1 && MODULAR_JDK=true || MODULAR_JDK=false
   if [ "$MODULAR_JDK" = "true" ]; then
-    DEFAULT_MODULAR_JVM_OPTIONS=`echo $1 | $GREP "\-\-add\-modules"`
+    DEFAULT_MODULAR_JVM_OPTIONS=`echo $* | $GREP "\-\-add\-modules"`
     if [ "x$DEFAULT_MODULAR_JVM_OPTIONS" = "x" ]; then
       # Set default modular jdk options
       DEFAULT_MODULAR_JVM_OPTIONS="$DEFAULT_MODULAR_JVM_OPTIONS --add-exports=java.base/sun.nio.ch=ALL-UNNAMED"
