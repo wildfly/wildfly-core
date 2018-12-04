edf4592140cb524f969d1524f88092b1e22a1f56
Brian Stansberry <brian.stansberry@redhat.com>
2018-12-04 14:18:51 -0600

    [WFCORE-4257] Replace RemotingModelControllerClient finalizer with a PhantomReference

--- controller-client/src/main/java/org/jboss/as/controller/client/impl/RemotingModelControllerClient.java
+++ controller-client/src/main/java/org/jboss/as/controller/client/impl/RemotingModelControllerClient.java
@@ -22,6 +22,7 @@
 
 package org.jboss.as.controller.client.impl;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.util.concurrent.TimeUnit;
@@ -37,6 +38,9 @@ import org.jboss.as.protocol.mgmt.ManagementClientChannelStrategy;
 import org.jboss.remoting3.Channel;
 import org.jboss.remoting3.CloseHandler;
 import org.jboss.remoting3.Endpoint;
+import org.wildfly.common.ref.CleanerReference;
+import org.wildfly.common.ref.Reaper;
+import org.wildfly.common.ref.Reference;
 
 /**
  * {@link ModelControllerClient} based on a Remoting {@link Endpoint}.
@@ -45,16 +49,37 @@ import org.jboss.remoting3.Endpoint;
  */
 public class RemotingModelControllerClient extends AbstractModelControllerClient {
 
-    private Endpoint endpoint;
-    private ManagementClientChannelStrategy strategy;
-    private boolean closed;
+    private static final Reaper<RemotingModelControllerClient, ClientCloseable> REAPER = new Reaper<RemotingModelControllerClient, ClientCloseable>() {
+        @Override
+        public void reap(Reference<RemotingModelControllerClient, ClientCloseable> reference) {
+            ClientCloseable closeable = reference.getAttachment();
+            if(! closeable.closed) {
+                // Create the leak description
+                final Throwable t = ControllerClientLogger.ROOT_LOGGER.controllerClientNotClosed();
+                t.setStackTrace(closeable.allocationStackTrace);
+                ControllerClientLogger.ROOT_LOGGER.leakedControllerClient(t);
+                // Close
+                StreamUtils.safeClose(closeable);
+            }
+        }
+    };
 
-    private final ManagementChannelHandler channelAssociation;
-    private final ModelControllerClientConfiguration clientConfiguration;
-    private final StackTraceElement[] allocationStackTrace;
+    public static RemotingModelControllerClient create(final ModelControllerClientConfiguration configuration) {
+        @SuppressWarnings("deprecation")
+        RemotingModelControllerClient client = new RemotingModelControllerClient(configuration);
+        // Use a PhantomReference instead of overriding finalize() to ensure close gets called
+        // CleanerReference handles ensuring there's a strong ref to itself so we can just construct it and move on
+        new CleanerReference<RemotingModelControllerClient, ClientCloseable>(client, client.closeable, REAPER);
+        return client;
+    }
 
+    private final ClientCloseable closeable;
+
+    /** @deprecated Use {@link #create(ModelControllerClientConfiguration)}  */
+    @Deprecated
     public RemotingModelControllerClient(final ModelControllerClientConfiguration configuration) {
-        this.channelAssociation = new ManagementChannelHandler(new ManagementClientChannelStrategy() {
+
+        ManagementChannelHandler handler = new ManagementChannelHandler(new ManagementClientChannelStrategy() {
             @Override
             public Channel getChannel() throws IOException {
                 return getOrCreateChannel();
@@ -65,100 +90,109 @@ public class RemotingModelControllerClient extends AbstractModelControllerClient
                 //
             }
         }, configuration.getExecutor(), this);
-        this.clientConfiguration = configuration;
-        this.allocationStackTrace = Thread.currentThread().getStackTrace();
+        this.closeable = new ClientCloseable(handler, configuration, Thread.currentThread().getStackTrace());
     }
 
     @Override
     protected ManagementChannelAssociation getChannelAssociation() throws IOException {
-        return channelAssociation;
+        return closeable.channelAssociation;
     }
 
     @Override
     public void close() throws IOException {
-        synchronized (this) {
-            if(closed) {
-                return;
+        closeable.close();
+    }
+
+    protected Channel getOrCreateChannel() throws IOException {
+        synchronized (closeable) {
+            if (closeable.closed) {
+                throw ControllerClientLogger.ROOT_LOGGER.objectIsClosed(ModelControllerClient.class.getSimpleName());
             }
-            closed = true;
-            // Don't allow any new request
-            channelAssociation.shutdown();
-            // First close the channel and connection
-            if (strategy != null) {
-                StreamUtils.safeClose(strategy);
-                strategy = null;
-            }
-            // Then the endpoint
-            final Endpoint endpoint = this.endpoint;
-            if (endpoint != null) {
-                this.endpoint = null;
+            if (closeable.strategy == null) {
                 try {
-                    endpoint.closeAsync();
-                } catch (UnsupportedOperationException ignored) {
+
+                    closeable.endpoint = Endpoint.builder().setEndpointName("management-client").build();
+
+                    final ProtocolConnectionConfiguration configuration = ProtocolConfigurationFactory.create(closeable.clientConfiguration, closeable.endpoint);
+
+                    closeable.strategy = ManagementClientChannelStrategy.create(configuration, closeable.channelAssociation, closeable.clientConfiguration.getCallbackHandler(),
+                            closeable.clientConfiguration.getSaslOptions(), closeable.clientConfiguration.getSSLContext(),
+                            new CloseHandler<Channel>() {
+                                @Override
+                                public void handleClose(final Channel closed, final IOException exception) {
+                                    closeable.channelAssociation.handleChannelClosed(closed, exception);
+                                }
+                            });
+                } catch (RuntimeException e) {
+                    throw e;
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
             }
-            // Cancel all still active operations
-            channelAssociation.shutdownNow();
-            try {
-                channelAssociation.awaitCompletion(1, TimeUnit.SECONDS);
-            } catch (InterruptedException ignore) {
-                Thread.currentThread().interrupt();
-            } finally {
-                StreamUtils.safeClose(clientConfiguration);
-            }
-            // Per WFCORE-1573 remoting endpoints should be closed asynchronously, however consumers of this client
-            // likely need to wait until the endpoints are fully shutdown.
-            if (endpoint != null) try {
-                endpoint.awaitClosed();
-            } catch (InterruptedException e) {
-                final InterruptedIOException cause = new InterruptedIOException(e.getLocalizedMessage());
-                cause.initCause(e);
-                throw cause;
-            }
+            return closeable.strategy.getChannel();
         }
     }
 
-    protected synchronized Channel getOrCreateChannel() throws IOException {
-        if (closed) {
-            throw ControllerClientLogger.ROOT_LOGGER.objectIsClosed(ModelControllerClient.class.getSimpleName());
+    private static final class ClientCloseable implements Closeable {
+
+        private Endpoint endpoint;
+        private ManagementClientChannelStrategy strategy;
+        private boolean closed;
+
+        private final ManagementChannelHandler channelAssociation;
+        private final ModelControllerClientConfiguration clientConfiguration;
+        private final StackTraceElement[] allocationStackTrace;
+
+        private ClientCloseable(ManagementChannelHandler channelAssociation,
+                                ModelControllerClientConfiguration clientConfiguration,
+                                StackTraceElement[] allocationStackTrace) {
+            this.channelAssociation = channelAssociation;
+            this.clientConfiguration = clientConfiguration;
+            this.allocationStackTrace = allocationStackTrace;
         }
-        if (strategy == null) {
-            try {
 
-                endpoint = Endpoint.builder().setEndpointName("management-client").build();
-
-                final ProtocolConnectionConfiguration configuration = ProtocolConfigurationFactory.create(clientConfiguration, endpoint);
-
-                strategy = ManagementClientChannelStrategy.create(configuration, channelAssociation, clientConfiguration.getCallbackHandler(),
-                        clientConfiguration.getSaslOptions(), clientConfiguration.getSSLContext(),
-                        new CloseHandler<Channel>() {
-                    @Override
-                    public void handleClose(final Channel closed, final IOException exception) {
-                        channelAssociation.handleChannelClosed(closed, exception);
+        @Override
+        public void close() throws IOException {
+            synchronized (this) {
+                if (closed) {
+                    return;
+                }
+                closed = true;
+                // Don't allow any new request
+                channelAssociation.shutdown();
+                // First close the channel and connection
+                if (strategy != null) {
+                    StreamUtils.safeClose(strategy);
+                    strategy = null;
+                }
+                // Then the endpoint
+                final Endpoint endpoint = this.endpoint;
+                if (endpoint != null) {
+                    this.endpoint = null;
+                    try {
+                        endpoint.closeAsync();
+                    } catch (UnsupportedOperationException ignored) {
                     }
-                });
-            } catch (RuntimeException e) {
-                throw e;
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+                }
+                // Cancel all still active operations
+                channelAssociation.shutdownNow();
+                try {
+                    channelAssociation.awaitCompletion(1, TimeUnit.SECONDS);
+                } catch (InterruptedException ignore) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    StreamUtils.safeClose(clientConfiguration);
+                }
+                // Per WFCORE-1573 remoting endpoints should be closed asynchronously, however consumers of this client
+                // likely need to wait until the endpoints are fully shutdown.
+                if (endpoint != null) try {
+                    endpoint.awaitClosed();
+                } catch (InterruptedException e) {
+                    final InterruptedIOException cause = new InterruptedIOException(e.getLocalizedMessage());
+                    cause.initCause(e);
+                    throw cause;
+                }
             }
-        }
-        return strategy.getChannel();
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        try {
-            if(! closed) {
-                // Create the leak description
-                final Throwable t = ControllerClientLogger.ROOT_LOGGER.controllerClientNotClosed();
-                t.setStackTrace(allocationStackTrace);
-                ControllerClientLogger.ROOT_LOGGER.leakedControllerClient(t);
-                // Close
-                StreamUtils.safeClose(this);
-            }
-        } finally {
-            super.finalize();
         }
     }
 
