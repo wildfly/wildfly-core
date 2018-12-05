e3f7fed6f9baa77530072ad466794fae6280e1d7
Chao Wang <chaowan@redhat.com>
2018-12-05 17:04:34 +0800

    [WFCORE-4239] WARN if system-property is already set and is being overridden

--- controller/src/main/java/org/jboss/as/controller/logging/ControllerLogger.java
+++ controller/src/main/java/org/jboss/as/controller/logging/ControllerLogger.java
@@ -3551,4 +3551,8 @@ public interface ControllerLogger extends BasicLogger {
 
     @Message(id = 455, value = "Can't take snapshot %s because it already exists")
     ConfigurationPersistenceException snapshotAlreadyExistError(String name);
+
+    @LogMessage(level = WARN)
+    @Message(id = 456, value = "System property %s=%s already set. It's being overridden by new value %s")
+    void systemPropertyAlreadyExist(String name, String value, String value2);
 }
