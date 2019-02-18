0f858afe6d7dcbd03484e9e484c9821c9ff27791
James Perkins <jperkins@redhat.com>
2019-02-18 10:31:49 -0800

    [WFCORE-4337] Bump the logging subsystem schema and model versions.

--- logging/src/main/java/org/jboss/as/logging/KnownModelVersion.java
+++ logging/src/main/java/org/jboss/as/logging/KnownModelVersion.java
@@ -15,6 +15,7 @@ public enum KnownModelVersion {
     VERSION_5_0_0(ModelVersion.create(5, 0, 0), true),
     VERSION_6_0_0(ModelVersion.create(6, 0, 0), true),
     VERSION_7_0_0(ModelVersion.create(7, 0, 0), false),
+    VERSION_8_0_0(ModelVersion.create(8, 0, 0), false),
     ;
     private final ModelVersion modelVersion;
     private final boolean hasTransformers;
