1f2c89d0def18fc868134b2b843dfeb45200e225
Brian Stansberry <brian.stansberry@redhat.com>
2019-02-26 18:58:24 -0600

    [WFCORE-4352] Avoid hangs from https://bugs.openjdk.java.net/browse/JDK-8219658

--- testsuite/standalone/src/test/java/org/jboss/as/test/integration/auditlog/AuditLogToTLSSyslogTestCase.java
+++ testsuite/standalone/src/test/java/org/jboss/as/test/integration/auditlog/AuditLogToTLSSyslogTestCase.java
@@ -17,6 +17,8 @@
  */
 package org.jboss.as.test.integration.auditlog;
 
+import org.junit.Assume;
+import org.junit.BeforeClass;
 import org.wildfly.core.testrunner.ServerSetup;
 import org.wildfly.core.testrunner.WildflyTestRunner;
 import org.junit.runner.RunWith;
@@ -30,5 +32,10 @@ import org.junit.runner.RunWith;
 //@RunAsClient
 @ServerSetup(AuditLogToTLSSyslogSetup.class)
 public class AuditLogToTLSSyslogTestCase extends AuditLogToSyslogTestCase {
+
+    @BeforeClass
+    public static void noJDK12() {
+        Assume.assumeFalse("Avoiding JDK 12 due to https://bugs.openjdk.java.net/browse/JDK-8219658", "12".equals(System.getProperty("java.specification.version")));
+    }
 
 }
