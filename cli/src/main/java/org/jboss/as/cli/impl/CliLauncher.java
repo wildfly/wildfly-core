00d90df2ba3d54d92388fe5cde1afab47206411b
asmirnov <asmirnov@solit-clouds.ru>
2019-06-04 09:56:20 +0300

    just setup jira ticket: WFCORE-4505

--- cli/src/main/java/org/jboss/as/cli/impl/CliLauncher.java
+++ cli/src/main/java/org/jboss/as/cli/impl/CliLauncher.java
@@ -21,11 +21,11 @@
  */
 package org.jboss.as.cli.impl;
 
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
