4d3dd113dbb1b273ccccc40e628e126452a754be
Farah Juma <fjuma@redhat.com>
2019-08-27 14:16:36 -0400

    [WFCORE-4150] Update the Elytron subsystem to credential-reference schema version 1.1

--- elytron/src/main/java/org/wildfly/extension/elytron/ElytronReloadRequiredWriteAttributeHandler.java
+++ elytron/src/main/java/org/wildfly/extension/elytron/ElytronReloadRequiredWriteAttributeHandler.java
@@ -23,11 +23,19 @@
 
 package org.wildfly.extension.elytron;
 
+import static org.jboss.as.controller.security.CredentialReference.applyCredentialReferenceUpdateToRuntime;
+import static org.jboss.as.controller.security.CredentialReference.handleCredentialReferenceUpdate;
+import static org.jboss.as.controller.security.CredentialReference.rollbackCredentialStoreUpdate;
+
 import java.util.Collection;
 
 import org.jboss.as.controller.AttributeDefinition;
 import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.ReloadRequiredWriteAttributeHandler;
+import org.jboss.as.controller.registry.Resource;
+import org.jboss.as.controller.security.CredentialReference;
+import org.jboss.dmr.ModelNode;
 
 /**
  * @author <a href="mailto:thofman@redhat.com">Tomas Hofman</a>
@@ -42,7 +50,34 @@ class ElytronReloadRequiredWriteAttributeHandler extends ReloadRequiredWriteAttr
     }
 
     @Override
+    protected void finishModelStage(OperationContext context, ModelNode operation, String attributeName, ModelNode newValue,
+                                    ModelNode oldValue, Resource resource) throws OperationFailedException {
+        super.finishModelStage(context, operation, attributeName, newValue, oldValue, resource);
+        if (attributeName.equals(CredentialReference.CREDENTIAL_REFERENCE)) {
+            handleCredentialReferenceUpdate(context, resource.getModel().get(attributeName), attributeName);
+        }
+    }
+
+    @Override
+    protected boolean applyUpdateToRuntime(OperationContext context, ModelNode operation, String attributeName, ModelNode resolvedValue,
+                                           ModelNode currentValue, HandbackHolder<Void> handbackHolder) throws OperationFailedException {
+        boolean requiresReload = false;
+        if (attributeName.equals(CredentialReference.CREDENTIAL_REFERENCE)) {
+            requiresReload = applyCredentialReferenceUpdateToRuntime(context, operation, resolvedValue, currentValue, attributeName);
+
+        }
+        return super.applyUpdateToRuntime(context, operation, attributeName, resolvedValue, currentValue, handbackHolder) || requiresReload;
+    }
+
+    @Override
     protected boolean requiresRuntime(final OperationContext context) {
         return isServerOrHostController(context);
+    }
+
+    @Override
+    protected void revertUpdateToRuntime(OperationContext context, ModelNode operation, String attributeName, ModelNode valueToRestore, ModelNode resolvedValue, Void handback) throws OperationFailedException {
+        if (attributeName.equals(CredentialReference.CREDENTIAL_REFERENCE)) {
+            rollbackCredentialStoreUpdate(getAttributeDefinition(attributeName), context, resolvedValue);
+        }
     }
 }
