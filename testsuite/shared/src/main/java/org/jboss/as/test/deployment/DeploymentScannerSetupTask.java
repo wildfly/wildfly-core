f7da21b13db63e6cfcd65d5620d0d42d33d71d43
Brian Stansberry <brian.stansberry@redhat.com>
2019-08-31 09:28:26 -0500

    [WFCORE-4642] Run testsuite/standalone test against a Galleon-slimmed server.

--- testsuite/shared/src/main/java/org/jboss/as/test/deployment/DeploymentScannerSetupTask.java
+++ testsuite/shared/src/main/java/org/jboss/as/test/deployment/DeploymentScannerSetupTask.java
@@ -20,14 +20,16 @@
  */
 package org.jboss.as.test.deployment;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CHILD_TYPE;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.EXTENSION;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.READ_CHILDREN_NAMES_OPERATION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RESULT;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUCCESS;
 import static org.junit.Assert.assertEquals;
 
 import org.jboss.as.controller.PathAddress;
-import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.operations.common.Util;
 import org.jboss.dmr.ModelNode;
 import org.wildfly.core.testrunner.ManagementClient;
@@ -38,27 +40,46 @@ import org.wildfly.core.testrunner.ServerSetupTask;
  * @author <a href="mailto:ehugonne@redhat.com">Emmanuel Hugonnet</a> (c) 2014 Red Hat, inc.
  */
 public class DeploymentScannerSetupTask implements ServerSetupTask {
-    private static final String DEPLOYMENT_SCANNER_EXTENSION = "org.jboss.as.deployment-scanner";
-    private static final String DEPLOYMENT_SCANNER_SUBSYSTEM = "deployment-scanner";
+    private static final PathAddress DEPLOYMENT_SCANNER_EXTENSION = PathAddress.pathAddress(EXTENSION, "org.jboss.as.deployment-scanner");
+    private static final PathAddress DEPLOYMENT_SCANNER_SUBSYSTEM = PathAddress.pathAddress(SUBSYSTEM, "deployment-scanner");
+
+    private boolean hasSubsytem;
 
     @Override
     public void setup(ManagementClient managementClient) throws Exception {
-        ModelNode addOp = Util.createAddOperation(PathAddress.pathAddress(PathElement.pathElement(EXTENSION, DEPLOYMENT_SCANNER_EXTENSION)));
-        ModelNode result = managementClient.getControllerClient().execute(addOp);
-        assertEquals("Unexpected outcome of adding the test deployment scanner extension: " + addOp, SUCCESS, result.get(OUTCOME).asString());
-        addOp = Util.createAddOperation(PathAddress.pathAddress(PathElement.pathElement(SUBSYSTEM, DEPLOYMENT_SCANNER_SUBSYSTEM)));
-        result = managementClient.getControllerClient().execute(addOp);
-        assertEquals("Unexpected outcome of adding the test deployment scanner subsystem: " + addOp, SUCCESS, result.get(OUTCOME).asString());
+
+        ModelNode op = Util.createEmptyOperation(READ_CHILDREN_NAMES_OPERATION, PathAddress.EMPTY_ADDRESS);
+        op.get(CHILD_TYPE).set(SUBSYSTEM);
+        ModelNode result = managementClient.getControllerClient().execute(op);
+        assertEquals("Unexpected outcome of checking for presence of the deployment scanner extension: " + op, SUCCESS, result.get(OUTCOME).asString());
+        for (ModelNode child : result.get(RESULT).asList()) {
+            if (child.asString().equals(DEPLOYMENT_SCANNER_SUBSYSTEM.getLastElement().getValue())) {
+                hasSubsytem = true;
+                break;
+            }
+        }
+
+        if (!hasSubsytem) {
+            ModelNode addOp = Util.createAddOperation(DEPLOYMENT_SCANNER_EXTENSION);
+            result = managementClient.getControllerClient().execute(addOp);
+            assertEquals("Unexpected outcome of adding the test deployment scanner extension: " + addOp, SUCCESS, result.get(OUTCOME).asString());
+            addOp = Util.createAddOperation(DEPLOYMENT_SCANNER_SUBSYSTEM);
+            result = managementClient.getControllerClient().execute(addOp);
+            assertEquals("Unexpected outcome of adding the test deployment scanner subsystem: " + addOp, SUCCESS, result.get(OUTCOME).asString());
+        }
     }
 
     @Override
     public void tearDown(ManagementClient managementClient) throws Exception {
-        try {
-            ModelNode removeOp = Util.createRemoveOperation(PathAddress.pathAddress(PathElement.pathElement(SUBSYSTEM, DEPLOYMENT_SCANNER_SUBSYSTEM)));
-            managementClient.getControllerClient().execute(removeOp);
-        } finally {
-            ModelNode removeOp = Util.createRemoveOperation(PathAddress.pathAddress(PathElement.pathElement(EXTENSION, DEPLOYMENT_SCANNER_EXTENSION)));
-            managementClient.getControllerClient().execute(removeOp);
+
+        if (!hasSubsytem) {
+            try {
+                ModelNode removeOp = Util.createRemoveOperation(DEPLOYMENT_SCANNER_SUBSYSTEM);
+                managementClient.getControllerClient().execute(removeOp);
+            } finally {
+                ModelNode removeOp = Util.createRemoveOperation(DEPLOYMENT_SCANNER_EXTENSION);
+                managementClient.getControllerClient().execute(removeOp);
+            }
         }
     }
 
