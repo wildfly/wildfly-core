b086f62f2c4388203d4108d5e87c8697661115a6
Farah Juma <fjuma@redhat.com>
2020-05-27 14:09:08 -0400

    [WFCORE-4987] Add version 11 of the WildFly Elytron subsystem schema and model

--- elytron/src/main/java/org/wildfly/extension/elytron/ElytronExtension.java
+++ elytron/src/main/java/org/wildfly/extension/elytron/ElytronExtension.java
@@ -62,6 +62,7 @@ public class ElytronExtension implements Extension {
     static final String NAMESPACE_8_0 = "urn:wildfly:elytron:8.0";
     static final String NAMESPACE_9_0 = "urn:wildfly:elytron:9.0";
     static final String NAMESPACE_10_0 = "urn:wildfly:elytron:10.0";
+    static final String NAMESPACE_11_0 = "urn:wildfly:elytron:11.0";
 
     /**
      * The name of our subsystem within the model.
@@ -84,8 +85,9 @@ public class ElytronExtension implements Extension {
     static final ModelVersion ELYTRON_8_0_0 = ModelVersion.create(8);
     static final ModelVersion ELYTRON_9_0_0 = ModelVersion.create(9);
     static final ModelVersion ELYTRON_10_0_0 = ModelVersion.create(10);
+    static final ModelVersion ELYTRON_11_0_0 = ModelVersion.create(11);
 
-    private static final ModelVersion ELYTRON_CURRENT = ELYTRON_10_0_0;
+    private static final ModelVersion ELYTRON_CURRENT = ELYTRON_11_0_0;
 
     static final String ISO_8601_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
@@ -127,6 +129,7 @@ public class ElytronExtension implements Extension {
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, NAMESPACE_8_0, () -> new ElytronSubsystemParser8_0());
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, NAMESPACE_9_0, () -> new ElytronSubsystemParser9_0());
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, NAMESPACE_10_0, () -> new ElytronSubsystemParser10_0());
+        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, NAMESPACE_11_0, () -> new ElytronSubsystemParser11_0());
     }
 
     @Override
@@ -139,7 +142,7 @@ public class ElytronExtension implements Extension {
         final ManagementResourceRegistration registration = subsystemRegistration.registerSubsystemModel(ElytronDefinition.INSTANCE);
         registration.registerOperationHandler(GenericSubsystemDescribeHandler.DEFINITION, GenericSubsystemDescribeHandler.INSTANCE);
 
-        subsystemRegistration.registerXMLElementWriter(() -> new ElytronSubsystemParser10_0());
+        subsystemRegistration.registerXMLElementWriter(() -> new ElytronSubsystemParser11_0());
     }
 
     @SuppressWarnings("unchecked")
--- elytron/src/test/java/org/wildfly/extension/elytron/ElytronSubsystem110TestCase.java
+++ elytron/src/test/java/org/wildfly/extension/elytron/ElytronSubsystem110TestCase.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2019 Red Hat, Inc., and individual contributors
+ * Copyright 2020 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,15 +26,15 @@ import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
  *
  * @author <a href="mailto:fjuma@redhat.com">Farah Juma</a>
  */
-public class ElytronSubsystem100TestCase extends AbstractSubsystemBaseTest {
+public class ElytronSubsystem110TestCase extends AbstractSubsystemBaseTest {
 
-    public ElytronSubsystem100TestCase() {
+    public ElytronSubsystem110TestCase() {
         super(ElytronExtension.SUBSYSTEM_NAME, new ElytronExtension());
     }
 
     @Override
     protected String getSubsystemXsdPath() throws Exception {
-        return "schema/wildfly-elytron_10_0.xsd";
+        return "schema/wildfly-elytron_11_0.xsd";
     }
 
     @Override
@@ -52,12 +52,7 @@ public class ElytronSubsystem100TestCase extends AbstractSubsystemBaseTest {
 
     @Override
     protected String getSubsystemXml() throws IOException {
-        return readResource("legacy-elytron-subsystem-10.0.xml");
-    }
-
-    @Override
-    protected void compareXml(String configId, String original, String marshalled) throws Exception {
-        //
+        return readResource("elytron-subsystem-11.0.xml");
     }
 
 }
