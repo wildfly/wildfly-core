5a232305ef725107767b7d4453957a94d8b6d406
Scott Marlow <smarlow@redhat.com>
2020-11-20 09:38:25 -0500

    [WFCORE-5198] Ignore deployment failures such as duplicate classes for addressing  issue.
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- server/src/main/java/org/jboss/as/server/deployment/DeploymentAddHandler.java
+++ server/src/main/java/org/jboss/as/server/deployment/DeploymentAddHandler.java
@@ -53,6 +53,7 @@ import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.controller.registry.Resource;
 import org.jboss.as.protocol.StreamUtils;
 import org.jboss.as.repository.ContentRepository;
+import org.jboss.as.server.controller.resources.DeploymentAttributes;
 import org.jboss.as.server.deployment.transformation.DeploymentTransformer;
 import org.jboss.as.server.logging.ServerLogger;
 import org.jboss.as.server.services.security.AbstractVaultReader;
@@ -217,7 +218,7 @@ public class DeploymentAddHandler implements OperationStepHandler {
         InputStream transformed = null;
         try {
             try {
-                transformed = deploymentTransformer == null ? in : deploymentTransformer.transform(in, name);
+                transformed = deploymentTransformer == null ? in : doTransformation(context, contentItemNode, name, in);
                 hash = contentRepository.addContent(transformed);
             } catch (IOException e) {
                 throw createFailureException(e.toString());
@@ -228,6 +229,25 @@ public class DeploymentAddHandler implements OperationStepHandler {
             StreamUtils.safeClose(transformed);
         }
         return new DeploymentHandlerUtil.ContentItem(hash);
+    }
+
+    private InputStream doTransformation(OperationContext context, ModelNode contentItemNode, String name, InputStream in) throws IOException, OperationFailedException {
+        try {
+            return deploymentTransformer.transform(in, name);
+        } catch (RuntimeException t) {
+            // Check if the InputStream is already attached to the operation request (as per CONTENT_INPUT_STREAM_INDEX check) and ignore that case
+            // as calling getInputStream would of returned the already partially consumed InputStream.
+            // Also verify that the thrown exception is the specific WFCORE-5198 `Error code 3`.
+            if (!contentItemNode.hasDefined(DeploymentAttributes.CONTENT_INPUT_STREAM_INDEX.getName()) &&
+                    t.getCause() != null && t.getCause().getCause() != null &&
+                    t.getCause().getCause() instanceof IOException &&
+                    t.getCause().getCause().getMessage().contains("during transformation. Error code 3")) {
+                ServerLogger.ROOT_LOGGER.tracef(t, "Ignoring transformation error and using original archive %s", name);
+                return getInputStream(context, contentItemNode);
+            } else {
+                throw t;
+            }
+        }
     }
 
     DeploymentHandlerUtil.ContentItem addUnmanaged(ModelNode contentItemNode) throws OperationFailedException {
