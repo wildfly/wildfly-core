c2a0544275f068df4ccccfad843eb86e551da42c
boris-unckel <bu.githubcom@mail.unckel.net>
2021-09-13 12:43:41 +0200

    [WFCORE-5575] Refactor Boolean parsing and Boolean object usage (jmx)
    Fixes https://issues.redhat.com/browse/WFCORE-5575

--- jmx/src/main/java/org/jboss/as/jmx/model/MBeanInfoFactory.java
+++ jmx/src/main/java/org/jboss/as/jmx/model/MBeanInfoFactory.java
@@ -392,8 +392,8 @@ public class MBeanInfoFactory {
     private Descriptor createAttributeDescriptor(ModelNode attribute) {
         Map<String, String> descriptions = new HashMap<String, String>();
         addMBeanExpressionSupport(descriptions);
-        Boolean allowExpressions = attribute.hasDefined(EXPRESSIONS_ALLOWED) && attribute.get(EXPRESSIONS_ALLOWED).asBoolean();
-        descriptions.put(DESC_EXPRESSIONS_ALLOWED, allowExpressions.toString());
+        boolean allowExpressions = attribute.hasDefined(EXPRESSIONS_ALLOWED) && attribute.get(EXPRESSIONS_ALLOWED).asBoolean();
+        descriptions.put(DESC_EXPRESSIONS_ALLOWED, Boolean.toString(allowExpressions));
         descriptions.put(DESC_EXPRESSIONS_ALLOWED_DESC, allowExpressions ?
                 JmxLogger.ROOT_LOGGER.descriptorAttributeExpressionsAllowedTrue() : JmxLogger.ROOT_LOGGER.descriptorAttributeExpressionsAllowedFalse());
         return new ImmutableDescriptor(descriptions);
