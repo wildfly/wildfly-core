786fe722da8778e78df03c986f704eff05ba6b78
Farah Juma <fjuma@redhat.com>
2021-10-06 18:19:54 -0400

    Revert "Revert "[WFCORE-5469] Add version 15 of the WildFly Elytron model and schema""
    This reverts commit 95be3cb6bbdb6ce9a36c174a04514b9955f0ef6a.

--- elytron/src/test/java/org/wildfly/extension/elytron/ElytronSubsystem15_0TestCase.java
+++ elytron/src/test/java/org/wildfly/extension/elytron/ElytronSubsystem15_0TestCase.java
@@ -20,35 +20,21 @@ package org.wildfly.extension.elytron;
 
 import java.io.IOException;
 
-import org.jboss.as.controller.RunningMode;
 import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
-import org.jboss.as.subsystem.test.AdditionalInitialization;
 
 /**
  *
- * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ * @author <a href="mailto:fjuma@redhat.com">Farah Juma</a>
  */
-public class ElytronSubsystem14_0TestCase extends AbstractSubsystemBaseTest {
+public class ElytronSubsystem15_0TestCase extends AbstractSubsystemBaseTest {
 
-    public ElytronSubsystem14_0TestCase() {
+    public ElytronSubsystem15_0TestCase() {
         super(ElytronExtension.SUBSYSTEM_NAME, new ElytronExtension());
     }
 
     @Override
     protected String getSubsystemXml() throws IOException {
-        return readResource("legacy-elytron-subsystem-14.0.xml");
-    }
-
-    @Override
-    protected void compareXml(String configId, String original, String marshalled) throws Exception {
-        //
-    }
-
-    @Override
-    protected AdditionalInitialization createAdditionalInitialization() {
-        // Our use of the expression=encryption resource requires kernel capability setup that TestEnvironment provides
-        return new TestEnvironment(RunningMode.ADMIN_ONLY);
+        return readResource("elytron-subsystem-15.0.xml");
     }
 
 }
-
