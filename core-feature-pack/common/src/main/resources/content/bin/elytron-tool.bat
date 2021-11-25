5c712af8b26b4aa5b65777ae4f3b116263425b9b
Ashpan Raskar <araskar@redhat.com>
2021-11-25 13:34:20 -0500

    [WFCORE-5712] Update elytron-tool.[bat|ps1] to make use of jboss-modules

--- core-feature-pack/common/src/main/resources/content/bin/elytron-tool.bat
+++ core-feature-pack/common/src/main/resources/content/bin/elytron-tool.bat
@@ -24,16 +24,20 @@ if "x%JBOSS_HOME%" == "x" (
   set "JBOSS_HOME=%RESOLVED_JBOSS_HOME%"
 )
 
+if "x%JBOSS_MODULEPATH%" == "x" (
+  set "JBOSS_MODULEPATH=%JBOSS_HOME%\modules"
+)
+
 pushd "%JBOSS_HOME%"
 set "SANITIZED_JBOSS_HOME=%CD%"
 popd
 
 if /i "%RESOLVED_JBOSS_HOME%" NEQ "%SANITIZED_JBOSS_HOME%" (
-   echo.
-   echo   WARNING:  JBOSS_HOME may be pointing to a different installation - unpredictable results may occur.
-   echo.
-   echo       JBOSS_HOME: "%JBOSS_HOME%"
-   echo.
+  echo.
+  echo   WARNING:  JBOSS_HOME may be pointing to a different installation - unpredictable results may occur.
+  echo.
+  echo       JBOSS_HOME: "%JBOSS_HOME%"
+  echo.
 )
 
 rem Setup JBoss specific properties
@@ -45,21 +49,29 @@ if "x%JAVA_HOME%" == "x" (
   set "JAVA=%JAVA_HOME%\bin\java"
 )
 
-rem Find wildfly-elytron-tool.jar, or we can't continue
-set "ELYTRON_TOOL_RUNJAR=%JBOSS_HOME%\bin\wildfly-elytron-tool.jar"
-if not exist "%ELYTRON_TOOL_RUNJAR%" (
-  echo Could not locate "%ELYTRON_TOOL_RUNJAR%".
+rem Find jboss-modules.jar, or we can't continue
+if not exist "%JBOSS_HOME%\jboss-modules.jar" (
+  echo Could not locate "%JBOSS_HOME%\jboss-modules.jar".
   echo Please check that you are in the bin directory when running this script.
   goto END
 )
 
+set JBOSS_CLI=%JBOSS_HOME%\bin\jboss-cli.bat
+set "DEPENDENCIES=java.logging,org.apache.commons.lang3,org.apache.commons.cli,org.apache.sshd,org.jboss.logging,org.jboss.logmanager,org.slf4j,org.wildfly.security.elytron-private,org.wildfly.common"
+set "MODULE_REMOVE_COMMAND=module remove --name=org.wildfly.security.elytron-tool-addons"
+set "MODULE_ADD_COMMAND=module add --name=org.wildfly.security.elytron-tool-addons --resources=%ELYTRON_TOOL_ADDONS% --dependencies=%DEPENDENCIES%"
+
 if not "x%ELYTRON_TOOL_ADDONS%" == "x" (
-   set ELYTRON_TOOL_SEP=;
+  if exist "%JBOSS_MODULEPATH%\org\wildfly\security\elytron-tool-addons" (
+
+    call %JBOSS_CLI% --command=^"%MODULE_REMOVE_COMMAND%^" > nul
+  )
+  call %JBOSS_CLI% --command=^"%MODULE_ADD_COMMAND%^" > nul
 )
 
-
 "%JAVA%" %JAVA_OPTS% ^
-    -cp "%ELYTRON_TOOL_RUNJAR%%ELYTRON_TOOL_SEP%%ELYTRON_TOOL_ADDONS%" org.wildfly.security.tool.ElytronTool ^
-     {%~nx0}%*
+  -jar "%JBOSS_HOME%\jboss-modules.jar" ^
+  -mp "%JBOSS_MODULEPATH%" org.wildfly.security.elytron-tool ^
+  %*
 
 :END
--- core-feature-pack/common/src/main/resources/content/bin/elytron-tool.ps1
+++ core-feature-pack/common/src/main/resources/content/bin/elytron-tool.ps1
@@ -17,13 +17,25 @@ if ($ARGS.Count -gt 0){
 
 $JAVA_OPTS = @()
 
+if (! $env:JBOSS_MODULEPATH) {
+  $JBOSS_MODULEPATH=$JBOSS_HOME + "\modules"
+}
+
 if ($env:ELYTRON_TOOL_ADDONS) {
-    $ELYTRON_TOOL_SEP = ";"
+  $JBOSS_CLI=$JBOSS_HOME + "\bin\jboss-cli.ps1"
+  # Same deps as elytron-tool module
+  $DEPENDENCIES="java.logging,org.apache.commons.lang3,org.apache.commons.cli,org.apache.sshd,org.jboss.logging,org.jboss.logmanager,org.slf4j,org.wildfly.security.elytron-private,org.wildfly.common"
+  if(Test-Path -Path ($JBOSS_MODULEPATH+"\org\wildfly\security\elytron-tool-addons")) {
+    $MODULE_REMOVE_COMMAND="module remove --name=org.wildfly.security.elytron-tool-addons";
+    & $JBOSS_CLI --command='"'$MODULE_REMOVE_COMMAND'"'
+  }
+  $MODULE_ADD_COMMAND="module add --name=org.wildfly.security.elytron-tool-addons --resources=$env:ELYTRON_TOOL_ADDONS --dependencies=$DEPENDENCIES"
+  & $JBOSS_CLI --command='"'$MODULE_ADD_COMMAND'"'
 }
 
 # Sample JPDA settings for remote socket debugging
 #$JAVA_OPTS+="-agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=y"
 
-& $JAVA $JAVA_OPTS -cp $JBOSS_HOME'\bin\wildfly-elytron-tool.jar'$ELYTRON_TOOL_SEP$env:ELYTRON_TOOL_ADDONS org.wildfly.security.tool.ElytronTool $ELYTRON_TOOL_OPTS
+& $JAVA $JAVA_OPTS -jar $JBOSS_HOME\jboss-modules.jar -mp $JBOSS_MODULEPATH org.wildfly.security.elytron-tool $args
 
 Env-Clean-Up
