65bfca727fb0a8b7df8b665f5802299d4216e645
Farah Juma <fjuma@redhat.com>
2021-11-26 11:27:28 -0500

    [WFCORE-5730] Un-ignore org.wildfly.scripts.test.ElytronToolScriptTestCase

--- testsuite/scripts/src/test/java/org/wildfly/scripts/test/ElytronToolScriptTestCase.java
+++ testsuite/scripts/src/test/java/org/wildfly/scripts/test/ElytronToolScriptTestCase.java
@@ -23,12 +23,10 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 
 /**
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
  */
-@Ignore("https://issues.jboss.org/browse/WFCORE-5729")
 public class ElytronToolScriptTestCase extends ScriptTestCase {
 
     public ElytronToolScriptTestCase() {
