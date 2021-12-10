50bbd648953ddd5d1d1d216699534813666325d1
petrberan <Peta.Beran@seznam.cz>
2021-12-10 15:33:22 +0100

    [WFCORE-5601] Update class javadoc for PluggableMBeanServerBuilder

--- jmx/src/main/java/org/jboss/as/jmx/PluggableMBeanServerBuilder.java
+++ jmx/src/main/java/org/jboss/as/jmx/PluggableMBeanServerBuilder.java
@@ -26,14 +26,16 @@ import javax.management.MBeanServerBuilder;
 import javax.management.MBeanServerDelegate;
 
 /**
- * <p>To use this builder, specify {@code -mbeanserverbuildermodule=org.jboss.as.jmx} when bootstrapping
- * jboss-modules, which in turn sets {@code -Djavax.management.builder.initial=org.jboss.as.jmx.PluggableMBeanServerBuilder}
- * (loaded from this module's {code META-INF/services/javax.management.MBeanServerBuilder}. This builder
- * returns an instance of {@link PluggableMBeanServerImpl} which can be used to set the MBeanServer chain, meaning that the
- * platform mbean server gets the extra functionality for TCCL, ModelController and whatever other behaviour we want to add.</p>
+ * <p> This builder returns an instance of {@link PluggableMBeanServerImpl} which can be used to set the MBeanServer chain,
+ * meaning that the platform mbean server gets the extra functionality for TCCL, ModelController and whatever other behaviour
+ * we want to add.</p>
  *
- * <p>If the {@code -mbeanserverbuildermodule} option is not specified, the additional behaviour is only added to
- * calls coming in via the remote connector or MBeanServers injected via a dependency on {@link MBeanServerService}.</p>
+ * <p> Main JBM module {@code org.jboss.modules.Main} loads services from {@code org.jboss.as.jmx} module, which includes
+ * {@code META-INF/services/javax.management.MBeanServerBuilder} where the {@code PluggableMBeanServerBuilder} is specified.</p>
+ *
+ * <p>If the {@code PluggableMBeanServerBuilder} in the {@code javax.management.MBeanServerBuilder} is not specified,
+ * the additional behaviour is only added to calls coming in via the remote connector or MBeanServers injected via
+ * a dependency on {@link MBeanServerService}.</p>
  *
  * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
  */
