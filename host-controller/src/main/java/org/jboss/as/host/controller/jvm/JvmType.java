5743a2a0cabf3777b550ed8143e5436147220fd4
rmartinc <rmartinc@redhat.com>
2022-06-09 08:25:54 +0200

    [WFCORE-5936] Ldap autentication using referrals fails on JDK 17 with ApacheDS

--- host-controller/src/main/java/org/jboss/as/host/controller/jvm/JvmType.java
+++ host-controller/src/main/java/org/jboss/as/host/controller/jvm/JvmType.java
@@ -60,6 +60,8 @@ public final class JvmType {
         // Keep them alphabetical to avoid the code history getting confused by reordering commits
         modularJavaOpts.add("--add-exports=java.desktop/sun.awt=ALL-UNNAMED");
         modularJavaOpts.add("--add-exports=java.naming/com.sun.jndi.ldap=ALL-UNNAMED");
+        modularJavaOpts.add("--add-exports=java.naming/com.sun.jndi.url.ldap=ALL-UNNAMED");
+        modularJavaOpts.add("--add-exports=java.naming/com.sun.jndi.url.ldaps=ALL-UNNAMED");
         modularJavaOpts.add("--add-opens=java.base/java.lang=ALL-UNNAMED");
         modularJavaOpts.add("--add-opens=java.base/java.lang.invoke=ALL-UNNAMED");
         modularJavaOpts.add("--add-opens=java.base/java.lang.reflect=ALL-UNNAMED");
