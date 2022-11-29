0ef150f3ffed7d9e187690a948e0a303b304d379
Richard Opálka <opalka.richard@gmail.com>
2022-11-29 09:52:55 +0100

    [WFCORE-6141] Fixing transient test failures

--- subsystem-test/tests/src/test/java/org/jboss/as/subsystem/test/otherservices/OtherServicesSubsystemTestCase.java
+++ subsystem-test/tests/src/test/java/org/jboss/as/subsystem/test/otherservices/OtherServicesSubsystemTestCase.java
@@ -88,7 +88,7 @@ public class OtherServicesSubsystemTestCase extends AbstractSubsystemTest {
 
         Assert.assertNotNull(services.getContainer().getService(OtherService.NAME));
         Assert.assertNotNull(services.getContainer().getService(MyService.NAME));
-        MyService myService = (MyService)services.getContainer().getService(MyService.NAME).getValue();
+        MyService myService = (MyService)services.getContainer().getService(MyService.NAME).awaitValue();
         Assert.assertNotNull(myService.otherValue.getValue());
     }
 
@@ -144,7 +144,7 @@ public class OtherServicesSubsystemTestCase extends AbstractSubsystemTest {
 
         ServiceController<?> controller = services.getContainer().getService(SocketBindingUserService.NAME);
         Assert.assertNotNull(controller);
-        SocketBindingUserService service = (SocketBindingUserService)controller.getValue();
+        SocketBindingUserService service = (SocketBindingUserService)controller.awaitValue();
         SocketBinding socketBinding = service.socketBindingValue.getValue();
         Assert.assertEquals(8000, socketBinding.getSocketBindings().getPortOffset());
         Assert.assertFalse("fixed port", socketBinding.isFixedPort());
@@ -189,7 +189,7 @@ public class OtherServicesSubsystemTestCase extends AbstractSubsystemTest {
 
         ServiceController<?> controller = services.getContainer().getService(SocketBindingUserService.NAME);
         Assert.assertNotNull(controller);
-        SocketBindingUserService service = (SocketBindingUserService)controller.getValue();
+        SocketBindingUserService service = (SocketBindingUserService)controller.awaitValue();
         SocketBinding socketBinding = service.socketBindingValue.getValue();
         Assert.assertEquals(234, socketBinding.getPort());
         Assert.assertEquals("127.0.0.1", socketBinding.getAddress().getHostAddress());
@@ -226,7 +226,7 @@ public class OtherServicesSubsystemTestCase extends AbstractSubsystemTest {
 
         ServiceController<?> controller = services.getContainer().getService(PathUserService.NAME);
         Assert.assertNotNull(controller);
-        PathUserService service = (PathUserService)controller.getValue();
+        PathUserService service = (PathUserService)controller.awaitValue();
         Assert.assertEquals(new File(".", "target").getAbsolutePath(), service.pathValue.getValue());
     }
 
