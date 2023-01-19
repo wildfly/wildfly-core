a0373280837d4cc8639db9535e67baa1da141299
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2023-01-19 12:26:55 -0600

    Remove `-server`/`-client` and `-d32`/`-d64` logic from scripts

--- core-feature-pack/common/src/main/resources/content/bin/domain.bat
+++ core-feature-pack/common/src/main/resources/content/bin/domain.bat
@@ -115,36 +115,6 @@ call "!DIRNAME!common.bat" :setSecurityManagerDefault
 set "PROCESS_CONTROLLER_JAVA_OPTS=!PROCESS_CONTROLLER_JAVA_OPTS! !SECURITY_MANAGER_CONFIG_OPTION!"
 set "HOST_CONTROLLER_JAVA_OPTS=!HOST_CONTROLLER_JAVA_OPTS! !SECURITY_MANAGER_CONFIG_OPTION!"
 
-rem Add -server to the JVM options, if supported by JDK
-echo "%JAVA_OPTS%" | findstr /I \-server > nul
-if errorlevel == 1 (
-    set JVM_OPTVERSION=-version
-    echo "%JAVA_OPTS%" | findstr /I \-d64 > nul
-    if not errorlevel == 1 (
-        set "JVM_OPTVERSION=-d64 !JVM_OPTVERSION!"
-        goto :CHECK_VERSION
-    )
-    echo "%JAVA_OPTS%" | findstr /I \-d32 > nul
-    if not errorlevel == 1 (
-        set "JVM_OPTVERSION=-d32 !JVM_OPTVERSION!"
-        goto :CHECK_VERSION
-    )
-    goto :CHECK_SUPPORTED
-
-    :CHECK_VERSION
-    "%JAVA%" !JVM_OPTVERSION! 2>&1 | findstr /I /C:"Unrecognized option" > nul
-    if not errorlevel == 1 (
-        set JVM_OPTVERSION=-version
-    )
-
-    rem Add -server to the JVM options, if supported
-    :CHECK_SUPPORTED
-    "%JAVA%" !JVM_OPTVERSION! 2>&1 | findstr /I /C:"hotspot" /C:"openJDK" /C:"IBM J9" > nul
-    if not errorlevel == 1 (
-        set "PROCESS_CONTROLLER_JAVA_OPTS=-server %PROCESS_CONTROLLER_JAVA_OPTS%"
-        set "HOST_CONTROLLER_JAVA_OPTS=-server %HOST_CONTROLLER_JAVA_OPTS%"
-    )
-)
 setlocal DisableDelayedExpansion
 
 rem Find run.jar, or we can't continue
--- core-feature-pack/common/src/main/resources/content/bin/standalone.ps1
+++ core-feature-pack/common/src/main/resources/content/bin/standalone.ps1
@@ -27,10 +27,6 @@ if ($SECMGR) {
     $MODULE_OPTS +="-secmgr";
 }
 
-if($PRESERVE_JAVA_OPTS -ne 'true') {
-    $JAVA_OPTS = Set-Java-Client-Option $JAVA_OPTS
-}
-
 # Set debug settings if not already set
 if ($global:DEBUG_MODE){
     if ($JAVA_OPTS -notcontains ('-agentlib:jdwp')){
