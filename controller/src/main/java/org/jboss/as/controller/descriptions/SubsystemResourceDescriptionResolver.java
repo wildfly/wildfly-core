1e52647ecb9a325a4ed9e0882b1b60f13e4ad625
Paul Ferraro <paul.ferraro@redhat.com>
2023-03-13 15:25:29 -0400

    WFCORE-6262 Allow child resolvers to create other child resolvers.

--- controller/src/main/java/org/jboss/as/controller/descriptions/SubsystemResourceDescriptionResolver.java
+++ controller/src/main/java/org/jboss/as/controller/descriptions/SubsystemResourceDescriptionResolver.java
@@ -15,9 +15,10 @@
  */
 package org.jboss.as.controller.descriptions;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.jboss.as.controller.Extension;
 import org.jboss.as.controller.PathElement;
 
 /**
@@ -28,26 +29,41 @@ public class SubsystemResourceDescriptionResolver extends StandardResourceDescri
 
     private static final String DEFAULT_RESOURCE_NAME = "LocalDescriptions";
 
+    private final String bundleName;
+    private final WeakReference<ClassLoader> bundleLoader;
+
     /**
      * Constructs a resolver of resource descriptions for the specified subsystem and extension class using the default resource name.
      * @param subsystemName a subsystem name
-     * @param extensionClass the extension class used to locate properties containing resource descriptions
+     * @param targetClass the extension or resource definition class used to locate properties containing resource descriptions
      */
-    public SubsystemResourceDescriptionResolver(String subsystemName, Class<? extends Extension> extensionClass) {
-        this(subsystemName, extensionClass, DEFAULT_RESOURCE_NAME);
+    public SubsystemResourceDescriptionResolver(String subsystemName, Class<?> targetClass) {
+        this(subsystemName, targetClass, DEFAULT_RESOURCE_NAME);
     }
 
     /**
      * Constructs a resolver of resource descriptions for the specified subsystem and extension class using the specified resource name.
      * @param subsystemName a subsystem name
-     * @param extensionClass the extension class used to locate properties containing resource descriptions
+     * @param targetClass the the extension or resource definition class used to locate properties containing resource descriptions
      */
-    public SubsystemResourceDescriptionResolver(String subsystemName, Class<? extends Extension> extensionClass, String resourceName) {
-        super(subsystemName, String.join(".", extensionClass.getPackage().getName(), resourceName), extensionClass.getClassLoader(), true, false);
+    public SubsystemResourceDescriptionResolver(String subsystemName, Class<?> targetClass, String resourceName) {
+        this(subsystemName, String.join(".", targetClass.getPackage().getName(), resourceName), targetClass.getClassLoader());
+    }
+
+    private SubsystemResourceDescriptionResolver(String prefix, String bundleName, ClassLoader bundleLoader) {
+        super(prefix, bundleName, bundleLoader, true, false);
+        this.bundleName = bundleName;
+        this.bundleLoader = new WeakReference<>(bundleLoader);
     }
 
     @Override
-    public ResourceDescriptionResolver createChildResolver(List<PathElement> paths) {
-        return new ChildResourceDescriptionResolver(this, this.getKeyPrefix(), paths);
+    public ParentResourceDescriptionResolver createChildResolver(PathElement path, List<PathElement> alternatePaths) {
+        List<ResourceDescriptionResolver> alternates = alternatePaths.isEmpty() ? List.of() : alternatePaths.stream().map(this::createResolver).collect(Collectors.toUnmodifiableList());
+        return new ChildResourceDescriptionResolver(this, this.createResolver(path), alternates);
+    }
+
+    private ParentResourceDescriptionResolver createResolver(PathElement path) {
+        List<CharSequence> keys = path.isWildcard() ? List.of(this.getKeyPrefix(), path.getKey()) : List.of(this.getKeyPrefix(), path.getKey(), path.getValue());
+        return new SubsystemResourceDescriptionResolver(String.join(".", keys), this.bundleName, this.bundleLoader.get());
     }
 }
