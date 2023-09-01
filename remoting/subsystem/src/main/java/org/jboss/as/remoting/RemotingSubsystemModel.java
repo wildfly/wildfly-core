1a261c444a094f5878d1cb2ba1bd8b1d36a89c27
Paul Ferraro <paul.ferraro@redhat.com>
2023-09-01 13:55:52 -0400

    Bump remoting subsystem model version to 7.0.0.

--- remoting/subsystem/src/main/java/org/jboss/as/remoting/RemotingSubsystemModel.java
+++ remoting/subsystem/src/main/java/org/jboss/as/remoting/RemotingSubsystemModel.java
@@ -26,8 +26,9 @@ public enum RemotingSubsystemModel implements SubsystemModel {
     VERSION_4_0_0(4, 0, 0), // WildFly 11, EAP 7.1
     VERSION_5_0_0(5, 0, 0), // WildFly 12 - 26, EAP 7.2 - 7.4
     VERSION_6_0_0(6, 0, 0), // WildFly 27 - present, EAP 8.0
+    VERSION_7_0_0(6, 0, 0), // WildFly 30
     ;
-    static final RemotingSubsystemModel CURRENT = VERSION_6_0_0;
+    static final RemotingSubsystemModel CURRENT = VERSION_7_0_0;
 
     private final ModelVersion version;
 
