527ab7997b773540c543be8ad76ea6d9c44878f3
xjusko <majus312@gmail.com>
2023-10-05 09:20:08 +0200

    [WFCORE-6532] Add security manager to script tests

--- testsuite/scripts/src/test/java/org/wildfly/scripts/test/DomainScriptTestCase.java
+++ testsuite/scripts/src/test/java/org/wildfly/scripts/test/DomainScriptTestCase.java
@@ -6,6 +6,9 @@
 package org.wildfly.scripts.test;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
@@ -15,22 +18,32 @@ import org.jboss.as.controller.client.helpers.Operations;
 import org.jboss.as.test.shared.TestSuiteEnvironment;
 import org.jboss.dmr.ModelNode;
 import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.wildfly.common.test.ServerHelper;
 
 /**
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
  */
+@RunWith(Parameterized.class)
 public class DomainScriptTestCase extends ScriptTestCase {
 
     private static final Function<ModelControllerClient, Boolean> HOST_CONTROLLER_CHECK = ServerHelper::isDomainRunning;
 
+    @Parameterized.Parameter
+    public Map<String, String> env;
     public DomainScriptTestCase() {
         super("domain");
     }
 
+    @Parameterized.Parameters
+    public static Collection<Object> data() {
+        return List.of(Map.of(), Map.of("SECMGR", "true"));
+    }
+
     @Override
     void testScript(final ScriptProcess script) throws InterruptedException, TimeoutException, IOException {
-        script.start(HOST_CONTROLLER_CHECK, ServerHelper.DEFAULT_SERVER_JAVA_OPTS);
+        script.start(HOST_CONTROLLER_CHECK, env, ServerHelper.DEFAULT_SERVER_JAVA_OPTS);
 
         Assert.assertNotNull("The process is null and may have failed to start.", script);
         Assert.assertTrue("The process is not running and should be", script.isAlive());
