a17b3b0201288c931d468ce32ea568b217399185
Paul Ferraro <paul.ferraro@redhat.com>
2023-12-21 17:56:22 -0500

    WFCORE-6347 Consolidate ResourceServiceValueRegistry and ServiceValueRegistry

--- subsystem/src/main/java/org/wildfly/subsystem/service/capture/ServiceValueRegistry.java
+++ subsystem/src/main/java/org/wildfly/subsystem/service/capture/ServiceValueRegistry.java
@@ -7,6 +7,7 @@ package org.wildfly.subsystem.service.capture;
 import java.util.function.Consumer;
 
 import org.wildfly.subsystem.service.ServiceDependency;
+import org.wildfly.subsystem.service.ServiceInstaller;
 
 /**
  * A registry of service values.
@@ -27,4 +28,25 @@ public interface ServiceValueRegistry<V> {
      * @param dependency a service dependency
      */
     void remove(ServiceDependency<V> dependency);
+
+    /**
+     * Creates a service installer to capture and release the value provided by the specified service dependency.
+     * @param dependency a service dependency
+     * @return a service installer
+     */
+    default ServiceInstaller capture(ServiceDependency<V> dependency) {
+        Consumer<V> startTask = new Consumer<>() {
+            @Override
+            public void accept(V value) {
+                ServiceValueRegistry.this.add(dependency).accept(value);
+            }
+        };
+        Consumer<V> stopTask = new Consumer<>() {
+            @Override
+            public void accept(V value) {
+                ServiceValueRegistry.this.remove(dependency);
+            }
+        };
+        return ServiceInstaller.builder(dependency).onStart(startTask).onStop(stopTask).asPassive().build();
+    }
 }
