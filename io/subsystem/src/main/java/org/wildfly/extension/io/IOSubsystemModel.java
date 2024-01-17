3e90d77127d144656870ec6ad6a3f669bc5dedd8
Paul Ferraro <paul.ferraro@redhat.com>
2024-01-17 18:27:13 -0500

    Increment IO subsystem model version.

--- io/subsystem/src/main/java/org/wildfly/extension/io/IOSubsystemModel.java
+++ io/subsystem/src/main/java/org/wildfly/extension/io/IOSubsystemModel.java
@@ -14,9 +14,10 @@ import org.jboss.as.controller.SubsystemModel;
 public enum IOSubsystemModel implements SubsystemModel {
 
     VERSION_4_0_0(4), // WildFly 12
-    VERSION_5_0_0(5), // WildFly 13-present, EAP 7.2-present
+    VERSION_5_0_0(5), // WildFly 13-31, EAP 7.2-8.0
+    VERSION_6_0_0(6), // WildFly 32-present
     ;
-    static final IOSubsystemModel CURRENT = VERSION_5_0_0;
+    static final IOSubsystemModel CURRENT = VERSION_6_0_0;
 
     private final ModelVersion version;
 
