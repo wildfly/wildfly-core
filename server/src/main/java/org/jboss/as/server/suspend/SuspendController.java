7b9ab0d85f599667e25d003a6d0abafc90c5a408
Brian Stansberry <brian.stansberry@redhat.com>
2024-02-26 15:45:35 -0600

    [WFLY-19067] Allow ordering of ServerActivity execution by registering them in distinct, ordered, 'execution groups'

--- server/src/main/java/org/jboss/as/server/suspend/SuspendController.java
+++ server/src/main/java/org/jboss/as/server/suspend/SuspendController.java
@@ -6,9 +6,14 @@
 package org.jboss.as.server.suspend;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NavigableMap;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.TreeMap;
+import java.util.function.BiConsumer;
+
 import org.jboss.as.controller.notification.NotificationHandlerRegistry;
 import org.jboss.as.server.logging.ServerLogger;
 import org.jboss.msc.service.Service;
@@ -16,6 +21,7 @@ import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
 import org.jboss.msc.value.InjectedValue;
+import org.wildfly.common.Assert;
 
 /**
  * The graceful shutdown controller. This class co-ordinates the graceful shutdown and pause/resume of a
@@ -23,7 +29,7 @@ import org.jboss.msc.value.InjectedValue;
  * <p/>
  * <p/>
  * In most cases this work is delegated to the request controller subsystem.
- * however for workflows that do no correspond directly to a request model a {@link ServerActivity} instance
+ * however for workflows that do not correspond directly to a request model a {@link ServerActivity} instance
  * can be registered directly with this controller.
  *
  * @author Stuart Douglas
@@ -37,13 +43,13 @@ public class SuspendController implements Service<SuspendController> {
 
     private State state = State.SUSPENDED;
 
-    private final List<ServerActivity> activities = new ArrayList<>();
+    private final NavigableMap<Integer, List<ServerActivity>> activitiesByGroup = new TreeMap<>();
 
     private final List<OperationListener> operationListeners = new ArrayList<>();
 
     private final InjectedValue<NotificationHandlerRegistry> notificationHandlerRegistry = new InjectedValue<>();
 
-    private int outstandingCount;
+    private int groupsCount;
 
     private boolean startSuspended;
 
@@ -75,20 +81,32 @@ public class SuspendController implements Service<SuspendController> {
         for(OperationListener listener: new ArrayList<>(operationListeners)) {
             listener.suspendStarted();
         }
-        outstandingCount = activities.size();
-        if (outstandingCount == 0) {
+        groupsCount = activitiesByGroup.size();
+        if (groupsCount == 0) {
             handlePause();
         } else {
-            CountingRequestCountCallback cb = new CountingRequestCountCallback(outstandingCount, () -> {
+            // Set up the logic that will handle the 'suspended' calls when all the preSuspend calls have reported 'done'
+            CountingRequestCountCallback preSuspendGroupCallBack = new CountingRequestCountCallback(groupsCount, () -> {
                 state = State.SUSPENDING;
-                for (ServerActivity activity : activities) {
-                    activity.suspended(SuspendController.this.listener);
-                }
+                processGroups(activitiesByGroup.values().iterator(), (executionGroup, cb) -> {
+                    for (ServerActivity activity : executionGroup) {
+                        // TODO considering making this concurrent by passing this call as a task to an executor.
+                        // This would allow each activity a "fair" share of the timeout budget
+                        // Alternatively we could iterate executionGroup in reverse (LIFO) order.
+                        // But the executionGroups themselves already provide an ability for that kind of ordering
+                        activity.suspended(cb);
+                    }
+                }, SuspendController.this.listener);
             });
 
-            for (ServerActivity activity : activities) {
-                activity.preSuspend(cb);
-            }
+            // Invoke the preSuspend calls
+            processGroups(activitiesByGroup.values().iterator(), (executionGroup, cb) -> {
+                for (ServerActivity activity : executionGroup) {
+                    // TODO see the 'suspended' section comment above re possible concurrent or LIFO execution
+                    activity.preSuspend(cb);
+                }
+            }, preSuspendGroupCallBack);
+
             if (timeoutMillis > 0) {
                 timer = new Timer();
                 timer.schedule(new TimerTask() {
@@ -125,24 +143,38 @@ public class SuspendController implements Service<SuspendController> {
             timer.cancel();
             timer = null;
         }
-        for(OperationListener listener: new ArrayList<>(operationListeners)) {
+        for (OperationListener listener : new ArrayList<>(operationListeners)) {
             listener.cancelled();
         }
-        for (ServerActivity activity : activities) {
-            try {
-                activity.resume();
-            } catch (Exception e) {
-                ServerLogger.ROOT_LOGGER.failedToResume(activity, e);
+        for (List<ServerActivity> executionGroup : activitiesByGroup.descendingMap().values()) {
+            for (ServerActivity activity : executionGroup) {
+                try {
+                    activity.resume();
+                } catch (Exception e) {
+                    ServerLogger.ROOT_LOGGER.failedToResume(activity, e);
+                }
             }
         }
         state = State.RUNNING;
     }
 
+    /**
+     * Registers the given {@link ServerActivity} with this controller
+     * @param activity the activity. Cannot be {@code null}
+     * @throws IllegalArgumentException if {@code activity} is {@code null} of if its
+     *                                  {@link ServerActivity#getExecutionGroup() getExecutionGroup()} method
+     *                                  returns a value outside of that method's documented legal range.
+     */
     public synchronized void registerActivity(final ServerActivity activity) {
-        this.activities.add(activity);
+        Assert.checkNotNullParam("activity", activity);
+        Assert.checkMinimumParameter("activity.getExecutionGroup()", ServerActivity.LOWEST_EXECUTION_GROUP, activity.getExecutionGroup());
+        Assert.checkMaximumParameter("activity.getExecutionGroup()", ServerActivity.HIGHEST_EXECUTION_GROUP, activity.getExecutionGroup());
+        List<ServerActivity> executionGroup = this.activitiesByGroup.computeIfAbsent(activity.getExecutionGroup(), ArrayList::new);
+        executionGroup.add(activity);
         if(state != State.RUNNING) {
             //if the activity is added when we are not running we just immediately suspend it
             //this should only happen at boot, so there should be no outstanding requests anyway
+            // note that this means there is no execution group grouping of these calls.
             activity.suspended(() -> {
 
             });
@@ -150,7 +182,13 @@ public class SuspendController implements Service<SuspendController> {
     }
 
     public synchronized void unRegisterActivity(final ServerActivity activity) {
-        this.activities.remove(activity);
+        List<ServerActivity> executionGroup = activitiesByGroup.get(activity.getExecutionGroup());
+        if (executionGroup != null) {
+            executionGroup.remove(activity);
+            if (executionGroup.isEmpty()) {
+                activitiesByGroup.remove(activity.getExecutionGroup());
+            }
+        }
     }
 
     @Override
@@ -169,12 +207,12 @@ public class SuspendController implements Service<SuspendController> {
     }
 
     private synchronized void activityPaused() {
-        --outstandingCount;
+        --groupsCount;
         handlePause();
     }
 
     private void handlePause() {
-        if (outstandingCount == 0) {
+        if (groupsCount == 0) {
             state = State.SUSPENDED;
             if (timer != null) {
                 timer.cancel();
@@ -213,6 +251,22 @@ public class SuspendController implements Service<SuspendController> {
 
     public InjectedValue<NotificationHandlerRegistry> getNotificationHandlerRegistry() {
         return notificationHandlerRegistry;
+    }
+
+    private void processGroups(Iterator<List<ServerActivity>> iterator,
+                               BiConsumer<List<ServerActivity>, ServerActivityCallback> groupFunction,
+                               ServerActivityCallback groupsCallback) {
+        // Take the first element from the iterator and apply the groupFunction, with a callback that
+        // calls this again to take the next element when all activities from the current element are done.
+        // When no elements are left, tell the groupsCallback we are done.
+        if (iterator.hasNext()) {
+            List<ServerActivity> activityList = iterator.next();
+            CountingRequestCountCallback cb = new CountingRequestCountCallback(activityList.size(), () -> {
+                processGroups(iterator, groupFunction, groupsCallback);
+                groupsCallback.done();
+            });
+            groupFunction.accept(activityList, cb);
+        }
     }
 
     public enum State {
