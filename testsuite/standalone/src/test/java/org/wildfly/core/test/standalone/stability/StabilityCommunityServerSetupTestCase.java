594b8c8bd37448220f40fe540b0c9dfb28652150
Kabir Khan <kkhan@redhat.com>
2024-03-18 16:16:22 +0000

    [WFCORE-6728] Make the stability setup tasks take/restore snapshots of the server config
    Otherwise, if we were working on a lower stability level, and made changes to the
    model, those could be stored with s schema from a lower stability level, meaning
    the reload back to the default community level will fail since it will contain
    xml elements from a namespace we can't handle

--- testsuite/standalone/src/test/java/org/wildfly/core/test/standalone/stability/StabilityCommunityServerSetupTestCase.java
+++ testsuite/standalone/src/test/java/org/wildfly/core/test/standalone/stability/StabilityCommunityServerSetupTestCase.java
@@ -21,14 +21,27 @@ package org.wildfly.core.test.standalone.stability;
 
 import org.jboss.as.version.Stability;
 import org.junit.runner.RunWith;
+import org.wildfly.core.testrunner.ManagementClient;
 import org.wildfly.core.testrunner.ServerSetup;
 import org.wildfly.core.testrunner.WildFlyRunner;
-import org.wildfly.test.stability.StabilityServerSetupTasks;
+import org.wildfly.test.stability.StabilityServerSetupSnapshotRestoreTasks;
 
-@ServerSetup(StabilityServerSetupTasks.Community.class)
+@ServerSetup(StabilityCommunityServerSetupTestCase.CommunityStabilitySetupTask.class)
 @RunWith(WildFlyRunner.class)
 public class StabilityCommunityServerSetupTestCase extends AbstractStabilityServerSetupTaskTest {
     public StabilityCommunityServerSetupTestCase() {
         super(Stability.COMMUNITY);
     }
+
+
+    public static class CommunityStabilitySetupTask extends StabilityServerSetupSnapshotRestoreTasks.Community {
+        @Override
+        protected void doSetup(ManagementClient managementClient) throws Exception {
+            // Not really needed since the resulting written xml will be of a higher stability level
+            // than the server. Still we are doing it for experimental preview, so it doesn't hurt to
+            // do the same here.
+            AbstractStabilityServerSetupTaskTest.addSystemProperty(managementClient, StabilityCommunityServerSetupTestCase.class);
+        }
+    }
+
 }
--- testsuite/standalone/src/test/java/org/wildfly/core/test/standalone/stability/StabilityDefaultServerSetupTestCase.java
+++ testsuite/standalone/src/test/java/org/wildfly/core/test/standalone/stability/StabilityDefaultServerSetupTestCase.java
@@ -21,15 +21,25 @@ package org.wildfly.core.test.standalone.stability;
 
 import org.jboss.as.version.Stability;
 import org.junit.runner.RunWith;
+import org.wildfly.core.testrunner.ManagementClient;
 import org.wildfly.core.testrunner.ServerSetup;
 import org.wildfly.core.testrunner.WildFlyRunner;
-import org.wildfly.test.stability.StabilityServerSetupTasks;
+import org.wildfly.test.stability.StabilityServerSetupSnapshotRestoreTasks;
 
-@ServerSetup(StabilityServerSetupTasks.Default.class)
+@ServerSetup(StabilityDefaultServerSetupTestCase.DefaultStabilitySetupTask.class)
 @RunWith(WildFlyRunner.class)
 public class StabilityDefaultServerSetupTestCase extends AbstractStabilityServerSetupTaskTest {
     public StabilityDefaultServerSetupTestCase() {
         super(Stability.DEFAULT);
     }
 
+    public static class DefaultStabilitySetupTask extends StabilityServerSetupSnapshotRestoreTasks.Default {
+        @Override
+        protected void doSetup(ManagementClient managementClient) throws Exception {
+            // Not really needed since the resulting written xml will be of a higher stability level
+            // than the server. Still we are doing it for experimental preview, so it doesn't hurt to
+            // do the same here.
+            AbstractStabilityServerSetupTaskTest.addSystemProperty(managementClient, StabilityDefaultServerSetupTestCase.class);
+        }
+    }
 }
