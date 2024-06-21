91c102642bb25b03cf26734c169663299035e1c2
Paul Ferraro <paul.ferraro@redhat.com>
2024-06-21 12:09:27 -0400

    WFCORE-6807 Prune deprecations from wildfly-service and wildfly-subsystem

--- subsystem/src/main/java/org/wildfly/subsystem/SubsystemPersistence.java
+++ subsystem/src/main/java/org/wildfly/subsystem/SubsystemPersistence.java
@@ -10,10 +10,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
 
 import org.jboss.as.controller.Feature;
-import org.jboss.as.controller.FeatureRegistry;
 import org.jboss.as.controller.PersistentResourceXMLDescription;
 import org.jboss.as.controller.PersistentResourceXMLDescriptionReader;
 import org.jboss.as.controller.PersistentResourceXMLDescriptionWriter;
@@ -45,16 +43,6 @@ public interface SubsystemPersistence<S extends SubsystemSchema<S>> {
      */
     default XMLElementReader<List<ModelNode>> getReader(S schema) {
         return schema;
-    }
-
-    /**
-     * Returns a StAX writer that writes the subsystem model to XML according to the current schema version.
-     * @return a StAX writer
-     * @deprecated Replaced by {@link #getWriter(FeatureRegistry)}
-     */
-    @Deprecated(forRemoval = true)
-    default XMLElementWriter<SubsystemMarshallingContext> getWriter() {
-        return this.getWriter(Stability.DEFAULT);
     }
 
     /**
@@ -138,70 +126,5 @@ public interface SubsystemPersistence<S extends SubsystemSchema<S>> {
                 return currentWriters.get(stability);
             }
         };
-    }
-
-    /**
-     * Creates the subsystem persistence configuration for the specified set of schemas, reader factory, and writer.
-     * @param <S> the schema type
-     * @param <R> the schema reader type
-     * @param schemas a set of schemas
-     * @param readerFactory a factory for creating an XML reader for a specific schema
-     * @param writer an XML reader
-     * @return a subsystem persistence configuration
-     * @deprecated Use {@link #of(Enum, XMLElementWriter) instead
-     */
-    @Deprecated(forRemoval = true)
-    static <S extends SubsystemSchema<S>, R extends XMLElementReader<List<ModelNode>>> SubsystemPersistence<S> of(Set<S> schemas, Function<S, R> readerFactory, XMLElementWriter<SubsystemMarshallingContext> writer) {
-        return new DefaultSubsystemPersistence<>(schemas, readerFactory, writer);
-    }
-
-    /**
-     * Creates the subsystem persistence configuration for the current version of the schema and specified writer
-     * @param <S> the schema type
-     * @param <R> the schema reader type
-     * @param schemas a set of schemas
-     * @param readerFactory a factory for creating an XML reader for a specific schema
-     * @param currentSchema the current schema version
-     * @param currentReader the XML reader for the current schema version
-     * @param writer an XML reader
-     * @return a subsystem persistence configuration
-     * @deprecated Use {@link #of(Enum, XMLElementWriter) instead
-     */
-    @Deprecated(forRemoval = true)
-    static <S extends SubsystemSchema<S>, R extends XMLElementReader<List<ModelNode>>> SubsystemPersistence<S> of(Set<S> schemas, Function<S, R> readerFactory, S currentSchema, XMLElementReader<List<ModelNode>> currentReader, XMLElementWriter<SubsystemMarshallingContext> writer) {
-        return new DefaultSubsystemPersistence<>(schemas, readerFactory, writer) {
-            @Override
-            public XMLElementReader<List<ModelNode>> getReader(S schema) {
-                return (schema == currentSchema) ? currentReader : readerFactory.apply(schema);
-            }
-        };
-    }
-
-    @Deprecated(forRemoval = true)
-    class DefaultSubsystemPersistence<S extends SubsystemSchema<S>, R extends XMLElementReader<List<ModelNode>>> implements SubsystemPersistence<S> {
-        private final Set<S> schemas;
-        private final Function<S, R> readerFactory;
-        private final XMLElementWriter<SubsystemMarshallingContext> writer;
-
-        DefaultSubsystemPersistence(Set<S> schemas, Function<S, R> readerFactory, XMLElementWriter<SubsystemMarshallingContext> writer) {
-            this.schemas = schemas;
-            this.readerFactory = readerFactory;
-            this.writer = writer;
-        }
-
-        @Override
-        public Set<S> getSchemas() {
-            return this.schemas;
-        }
-
-        @Override
-        public XMLElementReader<List<ModelNode>> getReader(S schema) {
-            return this.readerFactory.apply(schema);
-        }
-
-        @Override
-        public XMLElementWriter<SubsystemMarshallingContext> getWriter(Stability stability) {
-            return this.writer;
-        }
     }
 }
