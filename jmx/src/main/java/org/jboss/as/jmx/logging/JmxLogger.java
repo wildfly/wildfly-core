48debad85968125ebc02cea0cb792dcc6f0b529d
marko-bekhta <marko.prykladna@gmail.com>
2024-07-03 19:11:54 +0200

    [WFCORE-6883] Fix an issue with the RuntimeOperationsException requiring an exception parameter
    - the RuntimeOperationsException constructors require parameters. Without a "fake" parameter the generation of the logger will fail

--- jmx/src/main/java/org/jboss/as/jmx/logging/JmxLogger.java
+++ jmx/src/main/java/org/jboss/as/jmx/logging/JmxLogger.java
@@ -21,7 +21,6 @@ import javax.management.NotCompliantMBeanException;
 import javax.management.ObjectName;
 import javax.management.OperationsException;
 import javax.management.ReflectionException;
-import javax.management.RuntimeOperationsException;
 import javax.management.openmbean.OpenType;
 
 import org.jboss.as.controller.PathAddress;
@@ -445,7 +444,7 @@ public interface JmxLogger extends BasicLogger {
     String cannotUnregisterMBeansUnderReservedDomain(String domain);
 
     @Message(id = 46, value="The ObjectName coming from MBeanRegistration.preRegister() '%s' is in a reserved JMX domain")
-    RuntimeOperationsException badDomainInCalclulatedObjectNameException(ObjectName name);
+    IllegalArgumentException badDomainInCalculatedObjectNameException(ObjectName name);
 
     @LogMessage(level = ERROR)
     @Message(id = 47, value="An error happened unregistering the '%s' MBean registered in a reserved JMX domain")
