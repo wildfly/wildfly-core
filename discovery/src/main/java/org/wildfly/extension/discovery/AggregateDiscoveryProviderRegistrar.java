f90bbe9f70b9e7bd59eb66528ff3ca833e2392f9
Paul Ferraro <paul.ferraro@redhat.com>
2024-08-08 12:23:59 -0400

    Migrate to CapabilityReference-based attribute definitions.

--- discovery/src/main/java/org/wildfly/extension/discovery/AggregateDiscoveryProviderRegistrar.java
+++ discovery/src/main/java/org/wildfly/extension/discovery/AggregateDiscoveryProviderRegistrar.java
@@ -4,7 +4,6 @@
  */
 package org.wildfly.extension.discovery;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Supplier;
@@ -13,13 +12,12 @@ import org.jboss.as.controller.AttributeDefinition;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.PathElement;
-import org.jboss.as.controller.StringListAttributeDefinition;
-import org.jboss.as.controller.registry.AttributeAccess.Flag;
 import org.jboss.dmr.ModelNode;
 import org.wildfly.discovery.impl.AggregateDiscoveryProvider;
 import org.wildfly.discovery.spi.DiscoveryProvider;
 import org.wildfly.subsystem.resource.ResourceDescriptor;
 import org.wildfly.subsystem.resource.capability.CapabilityReference;
+import org.wildfly.subsystem.resource.capability.CapabilityReferenceListAttributeDefinition;
 import org.wildfly.subsystem.service.ResourceServiceInstaller;
 import org.wildfly.subsystem.service.ServiceDependency;
 import org.wildfly.subsystem.service.capability.CapabilityServiceInstaller;
@@ -32,10 +30,7 @@ public class AggregateDiscoveryProviderRegistrar extends DiscoveryProviderRegist
 
     static final PathElement PATH = PathElement.pathElement("aggregate-provider");
 
-    private static final StringListAttributeDefinition PROVIDER_NAMES = new StringListAttributeDefinition.Builder("providers")
-            .setCapabilityReference(CapabilityReference.builder(DISCOVERY_PROVIDER_CAPABILITY, DISCOVERY_PROVIDER_DESCRIPTOR).build())
-            .setFlags(Flag.RESTART_RESOURCE_SERVICES)
-            .build();
+    private static final CapabilityReferenceListAttributeDefinition<DiscoveryProvider> PROVIDER_NAMES = new CapabilityReferenceListAttributeDefinition.Builder<>("providers", CapabilityReference.builder(DISCOVERY_PROVIDER_CAPABILITY, DISCOVERY_PROVIDER_DESCRIPTOR).build()).build();
 
     static final Collection<AttributeDefinition> ATTRIBUTES = List.of(PROVIDER_NAMES);
 
@@ -45,19 +40,15 @@ public class AggregateDiscoveryProviderRegistrar extends DiscoveryProviderRegist
 
     @Override
     public ResourceServiceInstaller configure(OperationContext context, ModelNode model) throws OperationFailedException {
-        List<String> providers = PROVIDER_NAMES.unwrap(context, model);
-        List<ServiceDependency<DiscoveryProvider>> dependencies = new ArrayList<>(providers.size());
-        for (String provider : providers) {
-            dependencies.add(ServiceDependency.on(DISCOVERY_PROVIDER_DESCRIPTOR, provider));
-        }
+        List<ServiceDependency<DiscoveryProvider>> providers = PROVIDER_NAMES.resolve(context, model);
         Supplier<DiscoveryProvider> factory = new Supplier<>() {
             @Override
             public DiscoveryProvider get() {
-                return new AggregateDiscoveryProvider(dependencies.stream().map(Supplier::get).toArray(DiscoveryProvider[]::new));
+                return new AggregateDiscoveryProvider(providers.stream().map(Supplier::get).toArray(DiscoveryProvider[]::new));
             }
         };
         return CapabilityServiceInstaller.builder(DISCOVERY_PROVIDER_CAPABILITY, factory)
-                .requires(dependencies)
+                .requires(providers)
                 .build();
     }
 }
