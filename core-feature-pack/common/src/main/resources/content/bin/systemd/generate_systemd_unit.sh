bcf95283c2985e6f633e76cec0a7bf28e9337d8d
Yeray Borges <yborgess@redhat.com>
2024-10-15 17:54:23 +0100

    [WFCORE-6935] Correct naming for systemd, unit, file

--- core-feature-pack/common/src/main/resources/content/bin/systemd/generate_systemd_unit.sh
+++ core-feature-pack/common/src/main/resources/content/bin/systemd/generate_systemd_unit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This script is just a helper to generate the Systemd Unit Files for WildFly
+# This script is just a helper to generate the systemd unit files for WildFly
 # assuming that the server home will be the one that hold this script file.
 # It also allows to specify the user and group that will run the server.
 
@@ -19,7 +19,7 @@ SYSTEMD_FILE="${SCRIPT_DIR}/wildfly-${JBOSS_SYSTEMD_MODE}.service"
 JBOSS_SYSTEMD_USER="${2:-wildfly}"
 JBOSS_SYSTEMD_GROUP="${3:-wildfly}"
 
-echo "INFO: Systemd Unit File to generate: ${SYSTEMD_FILE}"
+echo "INFO: systemd unit file to generate: ${SYSTEMD_FILE}"
 echo "INFO: Using JBOSS_HOME: ${JBOSS_HOME}"
 echo "INFO: User: ${JBOSS_SYSTEMD_USER}"
 echo "INFO: Group: ${JBOSS_SYSTEMD_GROUP}"
@@ -41,10 +41,10 @@ sed -e "s|@@@JBOSS_SYSTEMD_SERVER_HOME@@@|${JBOSS_HOME}|g" \
 systemd-analyze verify --recursive-errors=no "${SYSTEMD_FILE}"
 
 echo ""
-echo "INFO: Systemd Unit File generated."
+echo "INFO: systemd unit file generated."
 echo "INFO: The ${JBOSS_SYSTEMD_USER}:${JBOSS_SYSTEMD_GROUP} are the user:group configured to launch the server. You have to ensure this user and group exist and have the necessary permissions to read and launch the server."
 echo "INFO: Use ${JBOSS_HOME}/bin/systemd/wildfly-${JBOSS_SYSTEMD_MODE}.conf to configure the server environment."
-echo "INFO: After configuring your environment, to install the server as a Systemd service do the following:"
+echo "INFO: After configuring your environment, to install the server as a systemd service do the following:"
 echo ""
 echo "sudo cp ${SYSTEMD_FILE} $(pkg-config systemd --variable=systemdsystemunitdir)"
 echo "sudo cp ${JBOSS_HOME}/bin/systemd/wildfly-${JBOSS_SYSTEMD_MODE}.conf /etc/sysconfig/wildfly-${JBOSS_SYSTEMD_MODE}.conf"
