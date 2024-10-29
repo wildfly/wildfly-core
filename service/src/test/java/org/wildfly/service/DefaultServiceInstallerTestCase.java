ba6891e7d7d75c8fa00ce13b7b273d1726a83e96
Paul Ferraro <paul.ferraro@redhat.com>
2024-10-29 16:23:41 +0000

    WFCORE-7039 Add Installer.Builder methods to run arbitrary tasks on service start/stop/removal.

--- service/src/test/java/org/wildfly/service/DefaultServiceInstallerTestCase.java
+++ service/src/test/java/org/wildfly/service/DefaultServiceInstallerTestCase.java
@@ -7,11 +7,18 @@ package org.wildfly.service;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 import org.jboss.msc.Service;
+import org.jboss.msc.service.LifecycleEvent;
+import org.jboss.msc.service.LifecycleListener;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceName;
@@ -38,12 +45,29 @@ public class DefaultServiceInstallerTestCase {
         ServiceDependency<Object> dependency = mock(ServiceDependency.class);
         Consumer<Object> captor = mock(Consumer.class);
         Consumer<Object> combinedCaptor = mock(Consumer.class);
-        Consumer<Object> startTask = mock(Consumer.class);
-        Consumer<Object> stopTask = mock(Consumer.class);
+        Consumer<Object> startConsumer = mock(Consumer.class);
+        Consumer<Object> stopConsumer = mock(Consumer.class);
         ArgumentCaptor<Service> service = ArgumentCaptor.forClass(Service.class);
         ArgumentCaptor<ServiceController.Mode> mode = ArgumentCaptor.forClass(ServiceController.Mode.class);
+        Map<LifecycleEvent, Runnable> tasks = new EnumMap<>(LifecycleEvent.class);
+        for (LifecycleEvent event : EnumSet.of(LifecycleEvent.UP, LifecycleEvent.DOWN, LifecycleEvent.REMOVED)) {
+            tasks.put(event, mock(Runnable.class));
+        }
 
-        ServiceInstaller installer = ServiceInstaller.builder(mapper, dependency).provides(name).requires(dependency).withCaptor(captor).onStart(startTask).onStop(stopTask).build();
+        ArgumentCaptor<LifecycleListener> capturedListener = ArgumentCaptor.forClass(LifecycleListener.class);
+
+        doReturn(builder).when(builder).addListener(capturedListener.capture());
+
+        ServiceInstaller installer = ServiceInstaller.builder(mapper, dependency)
+                .provides(name)
+                .requires(dependency)
+                .withCaptor(captor)
+                .onStart(startConsumer)
+                .onStop(stopConsumer)
+                .onStart(tasks.get(LifecycleEvent.UP))
+                .onStop(tasks.get(LifecycleEvent.DOWN))
+                .onRemove(tasks.get(LifecycleEvent.REMOVED))
+                .build();
 
         doReturn(builder).when(target).addService();
         doReturn(injector).when(builder).provides(name);
@@ -59,7 +83,26 @@ public class DefaultServiceInstallerTestCase {
 
         verify(dependency).accept(builder);
         verify(builder).provides(name);
+        for (Runnable task : tasks.values()) {
+            verifyNoInteractions(task);
+        }
 
-        DefaultServiceTestCase.test(service.getValue(), "value", "mappedValue", combinedCaptor, mapper, dependency, startTask, stopTask);
+        LifecycleListener listener = capturedListener.getValue();
+
+        Assert.assertNotNull(listener);
+
+        for (LifecycleEvent event : EnumSet.allOf(LifecycleEvent.class)) {
+            listener.handleEvent(controller, event);
+
+            for (Map.Entry<LifecycleEvent, Runnable> entry : tasks.entrySet()) {
+                if (event == entry.getKey()) {
+                    verify(entry.getValue()).run();
+                } else {
+                    verifyNoMoreInteractions(entry.getValue());
+                }
+            }
+        }
+
+        DefaultServiceTestCase.test(service.getValue(), "value", "mappedValue", combinedCaptor, mapper, dependency, startConsumer, stopConsumer);
     }
 }
