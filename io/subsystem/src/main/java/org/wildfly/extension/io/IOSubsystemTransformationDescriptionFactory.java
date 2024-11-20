744d147a2bbaace6eb463a880dde3bf38c5eabff
Paul Ferraro <paul.ferraro@redhat.com>
2024-11-20 20:29:46 +0000

    Migrate IO subsystem to SubsystemResourceXMLSchema.

--- io/subsystem/src/main/java/org/wildfly/extension/io/IOSubsystemTransformationDescriptionFactory.java
+++ io/subsystem/src/main/java/org/wildfly/extension/io/IOSubsystemTransformationDescriptionFactory.java
@@ -25,8 +25,8 @@ public enum IOSubsystemTransformationDescriptionFactory implements Function<Mode
         ResourceTransformationDescriptionBuilder builder = TransformationDescriptionBuilder.Factory.createSubsystemInstance();
         if (IOSubsystemModel.VERSION_6_0_0.requiresTransformation(version)) {
             builder.getAttributeBuilder()
-                .setDiscard(new DiscardAttributeChecker.DiscardAttributeValueChecker(IOSubsystemRegistrar.LEGACY_DEFAULT_WORKER), IOSubsystemRegistrar.DEFAULT_WORKER)
-                .addRejectCheck(RejectAttributeChecker.DEFINED, IOSubsystemRegistrar.DEFAULT_WORKER)
+                .setDiscard(new DiscardAttributeChecker.DiscardAttributeValueChecker(IOSubsystemResourceDefinitionRegistrar.LEGACY_DEFAULT_WORKER), IOSubsystemResourceDefinitionRegistrar.DEFAULT_WORKER)
+                .addRejectCheck(RejectAttributeChecker.DEFINED, IOSubsystemResourceDefinitionRegistrar.DEFAULT_WORKER)
                 .end();
         }
         return builder.build();
