d4f13c77c6a3b18b94812c0ab9ca9198fdc0d9f5
Paul Ferraro <paul.ferraro@redhat.com>
2024-12-05 12:07:11 +0000

    Align package name of tests with their source package.

--- controller/src/test/java/org/jboss/as/controller/operations/validation/ModuleNameValidatorTestCase.java
+++ controller/src/test/java/org/jboss/as/controller/operations/validation/ModuleNameValidatorTestCase.java
@@ -3,11 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.jboss.as.controller.operation.validation;
+package org.jboss.as.controller.operations.validation;
 
 import org.jboss.as.controller.OperationFailedException;
-import org.jboss.as.controller.operations.validation.ModuleNameValidator;
-import org.jboss.as.controller.operations.validation.ParameterValidator;
 import org.jboss.dmr.ModelNode;
 import org.junit.Assert;
 import org.junit.Test;
