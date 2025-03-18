ba0a5cf0da0115d5010b5eacd6ee1ed33f278239
Yeray Borges <yborgess@redhat.com>
2025-03-18 14:07:57 +0000

    [WFCORE-5718] Test rejects against legacy versions and transformers tests against EAP 8.0.0 Jira issue: https://issues.redhat.com/browse/WFCORE-5718

--- remoting/tests/src/test/java/org/jboss/as/remoting/RemotingSubsystemTransformerTestCase.java
+++ remoting/tests/src/test/java/org/jboss/as/remoting/RemotingSubsystemTransformerTestCase.java
@@ -4,17 +4,25 @@
  */
 package org.jboss.as.remoting;
 
+import static org.jboss.as.model.test.ModelTestControllerVersion.EAP_7_4_0;
+import static org.jboss.as.model.test.ModelTestControllerVersion.EAP_8_0_0;
 import static org.jboss.as.remoting.RemotingSubsystemTestUtil.DEFAULT_ADDITIONAL_INITIALIZATION;
+import static org.junit.Assert.assertTrue;
 
 import java.util.EnumSet;
+import java.util.List;
 
 import org.jboss.as.controller.ModelVersion;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.model.test.FailedOperationTransformationConfig;
 import org.jboss.as.model.test.ModelFixer;
 import org.jboss.as.model.test.ModelTestControllerVersion;
+import org.jboss.as.model.test.ModelTestUtils;
 import org.jboss.as.subsystem.test.AbstractSubsystemTest;
 import org.jboss.as.subsystem.test.AdditionalInitialization;
 import org.jboss.as.subsystem.test.KernelServices;
 import org.jboss.as.subsystem.test.KernelServicesBuilder;
+import org.jboss.dmr.ModelNode;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,7 +37,7 @@ public class RemotingSubsystemTransformerTestCase extends AbstractSubsystemTest 
 
     @Parameters
     public static Iterable<ModelTestControllerVersion> parameters() {
-        return EnumSet.of(ModelTestControllerVersion.EAP_7_4_0);
+        return EnumSet.of(EAP_8_0_0, EAP_7_4_0);
     }
 
     private final ModelTestControllerVersion controller;
@@ -39,7 +47,7 @@ public class RemotingSubsystemTransformerTestCase extends AbstractSubsystemTest 
     public RemotingSubsystemTransformerTestCase(ModelTestControllerVersion controller) {
         super(RemotingExtension.SUBSYSTEM_NAME, new RemotingExtension());
         this.controller = controller;
-        this.version = this.getModelVersion().getVersion();
+        this.version = this.getModelVersion();
     }
 
     private String formatSubsystemArtifact() {
@@ -50,10 +58,12 @@ public class RemotingSubsystemTransformerTestCase extends AbstractSubsystemTest 
         return String.format(pattern, this.controller.getCoreMavenGroupId(), this.controller.getCoreVersion());
     }
 
-    private RemotingSubsystemModel getModelVersion() {
+    private ModelVersion getModelVersion() {
         switch (this.controller) {
             case EAP_7_4_0:
-                return RemotingSubsystemModel.VERSION_5_0_0;
+                return EAP_7_4_0.getSubsystemModelVersion(getMainSubsystemName());
+            case EAP_8_0_0:
+                return EAP_8_0_0.getSubsystemModelVersion(getMainSubsystemName());
             default:
                 throw new IllegalArgumentException();
         }
@@ -62,7 +72,8 @@ public class RemotingSubsystemTransformerTestCase extends AbstractSubsystemTest 
     private String[] getDependencies() {
         switch (this.controller) {
             case EAP_7_4_0:
-                return new String[] {
+            case EAP_8_0_0:
+                return new String[]{
                         formatSubsystemArtifact(),
                 };
             default:
@@ -84,8 +95,8 @@ public class RemotingSubsystemTransformerTestCase extends AbstractSubsystemTest 
 
         KernelServices services = builder.build();
 
-        Assert.assertTrue(services.isSuccessfulBoot());
-        Assert.assertTrue(services.getLegacyServices(this.version).isSuccessfulBoot());
+        assertTrue(services.isSuccessfulBoot());
+        assertTrue(services.getLegacyServices(this.version).isSuccessfulBoot());
 
         ModelFixer fixer = model -> {
             if (RemotingSubsystemModel.VERSION_7_0_0.requiresTransformation(this.version)) {
@@ -97,5 +108,36 @@ public class RemotingSubsystemTransformerTestCase extends AbstractSubsystemTest 
 
         // check that both versions of the legacy model are the same and valid
         checkSubsystemModelTransformation(services, this.version, fixer, false);
+    }
+
+    @Test
+    public void testRejectingTransformers() throws Exception {
+        testRejectingTransformers(this.controller, this.version);
+    }
+
+    private void testRejectingTransformers(ModelTestControllerVersion controllerVersion, ModelVersion modelVersion) throws Exception {
+        //Boot up empty controllers with the resources needed for the ops coming from the xml to work
+        KernelServicesBuilder builder = createKernelServicesBuilder(DEFAULT_ADDITIONAL_INITIALIZATION);
+
+        builder.createLegacyKernelServicesBuilder(DEFAULT_ADDITIONAL_INITIALIZATION, controllerVersion, modelVersion)
+                .addMavenResourceURL(getDependencies())
+                .dontPersistXml();
+
+        KernelServices services = builder.build();
+        Assert.assertTrue(services.isSuccessfulBoot());
+        KernelServices legacyServices = services.getLegacyServices(modelVersion);
+        Assert.assertNotNull(legacyServices);
+        Assert.assertTrue(legacyServices.isSuccessfulBoot());
+
+        List<ModelNode> ops = builder.parseXmlResource("remoting-transform-rejects.xml");
+        PathAddress subsystemAddress = PathAddress.pathAddress("subsystem", RemotingExtension.SUBSYSTEM_NAME);
+
+        ModelTestUtils.checkFailedTransformedBootOperations(services, modelVersion, ops, new FailedOperationTransformationConfig()
+                .addFailedAttribute(subsystemAddress.append(ConnectorResource.PATH),
+                        new FailedOperationTransformationConfig
+                                .NewAttributesConfig(ConnectorResource.PROTOCOL)
+                )
+        );
+
     }
 }
