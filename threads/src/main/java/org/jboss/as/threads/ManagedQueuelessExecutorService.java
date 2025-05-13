c5dd042f08c39c749b571e35ae86130668410c65
Brian Stansberry <brian.stansberry@redhat.com>
2025-05-12 21:06:42 -0500

    [WFCORE-7238] Remove use of deprecated for removal BlockingExecutor

--- threads/src/main/java/org/jboss/as/threads/ManagedQueuelessExecutorService.java
+++ threads/src/main/java/org/jboss/as/threads/ManagedQueuelessExecutorService.java
@@ -4,31 +4,20 @@
  */
 package org.jboss.as.threads;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.TimeUnit;
-
-import org.jboss.threads.BlockingExecutor;
 import org.jboss.threads.EventListener;
-import org.jboss.threads.JBossExecutors;
 import org.jboss.threads.QueuelessExecutor;
 
 /**
  *
  * @author Alexey Loubyansky
  */
-public class ManagedQueuelessExecutorService extends ManagedExecutorService implements BlockingExecutor {
+public class ManagedQueuelessExecutorService extends ManagedExecutorService {
 
     private final QueuelessExecutor executor;
 
     public ManagedQueuelessExecutorService(QueuelessExecutor executor) {
         super(executor);
         this.executor = executor;
-    }
-
-    @Override
-    protected ExecutorService protectExecutor(ExecutorService executor) {
-        return JBossExecutors.protectedBlockingExecutorService((BlockingExecutor) executor);
     }
 
     @Override
@@ -78,23 +67,5 @@ public class ManagedQueuelessExecutorService extends ManagedExecutorService impl
 
     <A> void addShutdownListener(final EventListener<A> shutdownListener, final A attachment) {
         executor.addShutdownListener(shutdownListener, attachment);
-    }
-
-    @Override
-    public void executeBlocking(Runnable task)
-            throws RejectedExecutionException, InterruptedException {
-        executor.executeBlocking(task);
-    }
-
-    @Override
-    public void executeBlocking(Runnable task, long timeout, TimeUnit unit)
-            throws RejectedExecutionException, InterruptedException {
-        executor.executeBlocking(task, timeout, unit);
-    }
-
-    @Override
-    public void executeNonBlocking(Runnable task)
-            throws RejectedExecutionException {
-        executor.executeNonBlocking(task);
     }
 }
