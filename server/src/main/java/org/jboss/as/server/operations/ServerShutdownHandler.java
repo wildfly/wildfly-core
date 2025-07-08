3e6353b9322546ebb9dabd68dc5c3dace50d96c5
Paul Ferraro <paul.ferraro@redhat.com>
2025-07-08 18:00:51 +0100

    WFLY-20048 Shutdown operation should not block (potentially indefinitely) since it holds the ModelController lock. This otherwise prevents the user from issuing a resume operation to cancel a prolonged suspend.

--- server/src/main/java/org/jboss/as/server/operations/ServerShutdownHandler.java
+++ server/src/main/java/org/jboss/as/server/operations/ServerShutdownHandler.java
@@ -20,10 +20,9 @@ import java.io.FileReader;
 import java.nio.file.Path;
 import java.util.EnumSet;
 import java.util.Properties;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BiConsumer;
 
 import org.jboss.as.controller.ControlledProcessState;
 import org.jboss.as.controller.OperationContext;
@@ -147,18 +146,12 @@ public class ServerShutdownHandler implements OperationStepHandler {
                             //even if the timeout is zero we still pause the server
                             //to stop new requests being accepted as it is shutting down
                             final ShutdownAction shutdown = new ShutdownAction(getOperationName(operation), restart, performInstallation);
-                            final ServerSuspendController suspendController = ServerShutdownHandler.this.suspendController;
                             ServerLogger.ROOT_LOGGER.suspendingServer(seconds, TimeUnit.SECONDS);
-                            CompletableFuture<Void> suspend = suspendController.suspend(ServerSuspendController.Context.SHUTDOWN).toCompletableFuture();
+                            CompletionStage<Void> suspend = ServerShutdownHandler.this.suspendController.suspend(ServerSuspendController.Context.SHUTDOWN);
                             if (seconds >= 0) {
-                                suspend.completeOnTimeout(null, seconds, TimeUnit.SECONDS);
+                                suspend.toCompletableFuture().completeOnTimeout(null, seconds, TimeUnit.SECONDS);
                             }
-                            try {
-                                suspend.join();
-                                shutdown.shutdown();
-                            } catch (CancellationException e) {
-                                shutdown.cancel();
-                            }
+                            suspend.whenComplete(shutdown);
                         }
                     }
                 });
@@ -170,7 +163,7 @@ public class ServerShutdownHandler implements OperationStepHandler {
         return op.hasDefined(OP) ? op.get(OP).asString() : SHUTDOWN;
     }
 
-    private final class ShutdownAction extends AtomicBoolean {
+    private final class ShutdownAction implements BiConsumer<Void, Throwable> {
 
         private final String op;
         private final boolean restart;
@@ -182,12 +175,11 @@ public class ServerShutdownHandler implements OperationStepHandler {
             this.performInstallation = performInstallation;
         }
 
-        void cancel() {
-            compareAndSet(false, true);
-        }
-
-        void shutdown() {
-            if(compareAndSet(false, true)) {
+        @Override
+        public void accept(Void ignore, Throwable exception) {
+            if (exception != null) {
+                ServerLogger.ROOT_LOGGER.caughtExceptionDuringShutdown(exception);
+            } else {
                 processState.setStopping();
                 final Thread thread = new Thread(new Runnable() {
                     public void run() {
