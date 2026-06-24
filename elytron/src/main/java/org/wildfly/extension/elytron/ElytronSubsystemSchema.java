e9154756347c8542108853d3ae3197c3f86a1707
Charlie Zhang <charliezhangbusiness@gmail.com>
2026-06-24 01:08:04 +0100

    [WFCORE-7565] Add ACME external account binding support

--- elytron/src/main/java/org/wildfly/extension/elytron/ElytronSubsystemSchema.java
+++ elytron/src/main/java/org/wildfly/extension/elytron/ElytronSubsystemSchema.java
@@ -55,8 +55,10 @@ public enum ElytronSubsystemSchema implements PersistentSubsystemSchema<ElytronS
     VERSION_18_0(18),
     VERSION_18_0_COMMUNITY(18, Stability.COMMUNITY),
     VERSION_19_0(19),
+    VERSION_19_0_PREVIEW(19, Stability.PREVIEW),
     ;
-    static final Map<Stability, ElytronSubsystemSchema> CURRENT = Feature.map(EnumSet.of(VERSION_19_0));
+    static final Map<Stability, ElytronSubsystemSchema> CURRENT = Feature.map(EnumSet.of(VERSION_19_0,
+            VERSION_19_0_PREVIEW));
 
     private final VersionedNamespace<IntVersion, ElytronSubsystemSchema> namespace;
 
@@ -193,7 +195,9 @@ public enum ElytronSubsystemSchema implements PersistentSubsystemSchema<ElytronS
 
     private void addTlsParser(PersistentResourceXMLDescription.PersistentResourceXMLBuilder builder) {
         TlsParser tlsParser = new TlsParser();
-        if (this.since(ElytronSubsystemSchema.VERSION_19_0)) {
+        if (this.since(ElytronSubsystemSchema.VERSION_19_0_PREVIEW)) {
+            builder.addChild(tlsParser.tlsParserPreview_19_0);
+        } else if (this.since(ElytronSubsystemSchema.VERSION_19_0)) {
             builder.addChild(tlsParser.tlsParser_19_0);
         } else if (this.since(ElytronSubsystemSchema.VERSION_18_0_COMMUNITY) && this.enables(getDynamicClientSSLContextDefinition())) {
             builder.addChild(tlsParser.tlsParserCommunity_18_0);
