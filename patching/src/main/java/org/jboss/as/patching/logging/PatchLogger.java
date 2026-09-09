fb621815c302cfc9ccb5d39512ccaee46b41e7c3
Brian Stansberry <bstansbe@ibm.com>
2026-09-09 15:23:58 -0500

    [WFCORE-7695] Remove unused IdentityPatchRunner and then the unused patching task code

--- patching/src/main/java/org/jboss/as/patching/logging/PatchLogger.java
+++ patching/src/main/java/org/jboss/as/patching/logging/PatchLogger.java
@@ -5,17 +5,12 @@
 
 package org.jboss.as.patching.logging;
 
-import static org.jboss.logging.Logger.Level.ERROR;
 import static org.jboss.logging.Logger.Level.WARN;
 
 import java.io.IOException;
-import java.io.SyncFailedException;
 import java.lang.invoke.MethodHandles;
-import java.util.Collection;
 
-import org.jboss.as.patching.ContentConflictsException;
 import org.jboss.as.patching.PatchingException;
-import org.jboss.as.patching.metadata.ContentItem;
 import org.jboss.as.patching.validation.PatchingArtifact;
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.Logger;
@@ -24,7 +19,6 @@ import org.jboss.logging.annotations.Cause;
 import org.jboss.logging.annotations.LogMessage;
 import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
-import org.jboss.logging.annotations.Param;
 
 /**
  * @author Emanuel Muckenhuber
@@ -67,16 +61,16 @@ public interface PatchLogger extends BasicLogger {
     @Message(id = Message.NONE, value = "patch types don't match")
     IllegalStateException patchTypesDontMatch();
 
-    @Message(id = Message.NONE, value = "invalid rollback information")
-    PatchingException invalidRollbackInformation();
+//    @Message(id = Message.NONE, value = "invalid rollback information")
+//    PatchingException invalidRollbackInformation();
 
     // User related errors
 
-    @Message(id = 3, value = "Patch does not apply - expected (%s), but was (%s)")
-    PatchingException doesNotApply(String appliesTo, String version);
+//    @Message(id = 3, value = "Patch does not apply - expected (%s), but was (%s)")
+//    PatchingException doesNotApply(String appliesTo, String version);
 
-    @Message(id = 4, value = "Failed to delete (%s)")
-    IOException failedToDelete(String path);
+//    @Message(id = 4, value = "Failed to delete (%s)")
+//    IOException failedToDelete(String path);
 
     @Message(id = 5, value = "Failed to create directory (%s)")
     IOException cannotCreateDirectory(String path);
@@ -94,8 +88,8 @@ public interface PatchLogger extends BasicLogger {
 //    @Message(id = 7, value = "Missing required argument(s): %s")
 //    String missingRequiredArgs(Set<String> missing);
 
-    @Message(id = 8, value = "File at path specified by argument %s does not exist")
-    String fileDoesNotExist(String arg);
+//    @Message(id = 8, value = "File at path specified by argument %s does not exist")
+//    String fileDoesNotExist(String arg);
 
 //    @Message(id = 9, value = "File at path specified by argument %s is not a directory")
 //    String fileIsNotADirectory(String arg);
@@ -109,29 +103,29 @@ public interface PatchLogger extends BasicLogger {
     @Message(id = 12, value = "Patch '%s' already applied")
     PatchingException alreadyApplied(String patchId);
 
-    @Message(id = 13, value = "There is no layer called %s installed")
-    PatchingException noSuchLayer(String name);
+//    @Message(id = 13, value = "There is no layer called %s installed")
+//    PatchingException noSuchLayer(String name);
 
-    @Message(id = 14, value = "Failed to resolve a valid patch descriptor for %s %s")
-    PatchingException failedToResolvePatch(String product, String version);
+//    @Message(id = 14, value = "Failed to resolve a valid patch descriptor for %s %s")
+//    PatchingException failedToResolvePatch(String product, String version);
 
-    @Message(id = 15, value = "Requires patch '%s'")
-    PatchingException requiresPatch(String patchId);
+//    @Message(id = 15, value = "Requires patch '%s'")
+//    PatchingException requiresPatch(String patchId);
 
-    @Message(id = 16, value = "Patch is incompatible with patch '%s'")
-    PatchingException incompatiblePatch(String patchId);
+//    @Message(id = 16, value = "Patch is incompatible with patch '%s'")
+//    PatchingException incompatiblePatch(String patchId);
 
-    @Message(id = 17, value = "Conflicts detected")
-    ContentConflictsException conflictsDetected(@Param Collection<ContentItem> conflicts);
+//    @Message(id = 17, value = "Conflicts detected")
+//    ContentConflictsException conflictsDetected(@Param Collection<ContentItem> conflicts);
 
-    @Message(id = 18, value = "copied content does not match expected hash for item: %s")
-    SyncFailedException wrongCopiedContent(ContentItem item);
+//    @Message(id = 18, value = "copied content does not match expected hash for item: %s")
+//    SyncFailedException wrongCopiedContent(ContentItem item);
 
     @Message(id = 19, value = "invalid patch name '%s'")
     IllegalArgumentException illegalPatchName(String name);
 
-    @Message(id = 20, value = "Cannot rollback. No patches applied.")
-    IllegalArgumentException noPatchesApplied();
+//    @Message(id = 20, value = "Cannot rollback. No patches applied.")
+//    IllegalArgumentException noPatchesApplied();
 
     @Message(id = 21, value = "Patch '%s' not found in history.")
     PatchingException patchNotFoundInHistory(String patchId);
@@ -163,9 +157,9 @@ public interface PatchLogger extends BasicLogger {
     @Message(id = 30, value = "Layer not found %s")
     String layerNotFound(String name);
 
-    @LogMessage(level = ERROR)
-    @Message(id = 31, value = "failed to undo change for: '%s'")
-    void failedToUndoChange(String name);
+//    @LogMessage(level = ERROR)
+//    @Message(id = 31, value = "failed to undo change for: '%s'")
+//    void failedToUndoChange(String name);
 
     @Message(id = 32, value = "missing: '%s'")
     String missingArtifact(PatchingArtifact.ArtifactState state);
@@ -195,11 +189,11 @@ public interface PatchLogger extends BasicLogger {
     @Message(id = 40, value = "failed to load %s info")
     String failedToLoadInfo(String name);
 
-    @Message(id = 41, value = "Patch %s found in more than one stream: %s and %s")
-    String patchIdFoundInMoreThanOneStream(String id, String stream1, String stream2);
+//    @Message(id = 41, value = "Patch %s found in more than one stream: %s and %s")
+//    String patchIdFoundInMoreThanOneStream(String id, String stream1, String stream2);
 
-    @Message(id = 42, value = "Patch bundle is empty")
-    String patchBundleIsEmpty();
+//    @Message(id = 42, value = "Patch bundle is empty")
+//    String patchBundleIsEmpty();
 
     @Message(id = 43, value = "Content item type is missing in '%s'")
     PatchingException contentItemTypeMissing(String condition);
@@ -210,8 +204,8 @@ public interface PatchLogger extends BasicLogger {
     @Message(id = 45, value = "Unrecognized condition format '%s'")
     PatchingException unrecognizedConditionFormat(String condition);
 
-    @Message(id = 46, value = "Cannot copy files to temporary directory %s: %s. Note that '-Djava.io.tmpdir' switch can be used to set different temporary directory.")
-    PatchingException cannotCopyFilesToTempDir(String tempDir, String reason, @Cause Throwable cause);
+//    @Message(id = 46, value = "Cannot copy files to temporary directory %s: %s. Note that '-Djava.io.tmpdir' switch can be used to set different temporary directory.")
+//    PatchingException cannotCopyFilesToTempDir(String tempDir, String reason, @Cause Throwable cause);
 
     @Message(id = 47, value = "Cannot copy files from %s to %s: %s")
     IOException cannotCopyFiles(String from, String to, String reason, @Cause Throwable cause);
